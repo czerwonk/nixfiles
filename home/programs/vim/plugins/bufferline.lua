935237984764c40d507c942056783d423e217c31
Daniel Czerwonk <czerwonk@mauve.de>
2023-04-21 11:23:00 +0200

    fix bufferline config

--- home/programs/vim/plugins/bufferline.lua
+++ home/programs/vim/plugins/bufferline.lua
@@ -1,6 +1,6 @@
 require("bufferline").setup{
-  diagnostics = "nvim_lsp",
   options = {
+    diagnostics = "nvim_lsp",
     buffer_close_icon = "",
     close_command = "bdelete %d",
     close_icon = "",
@@ -10,7 +10,7 @@ require("bufferline").setup{
     },
     left_trunc_marker = "",
     modified_icon = "●",
-    offsets = { 
+    offsets = {
       { filetype = "NvimTree", text = "EXPLORER", text_align = "center" }
     },
     right_mouse_command = "bdelete! %d",
