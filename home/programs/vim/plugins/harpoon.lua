5afa233ccfca2275ccc6533f1fd4a8c06566f721
Daniel Czerwonk <czerwonk@mauve.de>
2023-04-25 12:35:24 +0200

    enable harpoon telescope extension

--- home/programs/vim/plugins/harpoon.lua
+++ home/programs/vim/plugins/harpoon.lua
@@ -3,6 +3,8 @@ require("harpoon").setup {
     save_on_toggle = true,
   },
 }
+require("telescope").load_extension('harpoon')
+
 local harpoonMark = require("harpoon.mark")
 local harpoonUI = require("harpoon.ui")
 vim.keymap.set("n", "<leader>ma", harpoonMark.add_file, { desc = 'Add File' })
@@ -13,3 +15,4 @@ vim.keymap.set("n", "<leader>m1", function() harpoonUI.nav_file(1) end, { desc =
 vim.keymap.set("n", "<leader>m2", function() harpoonUI.nav_file(2) end, { desc = 'File 2' })
 vim.keymap.set("n", "<leader>m3", function() harpoonUI.nav_file(3) end, { desc = 'File 3' })
 vim.keymap.set("n", "<leader>m4", function() harpoonUI.nav_file(4) end, { desc = 'File 4' })
+
