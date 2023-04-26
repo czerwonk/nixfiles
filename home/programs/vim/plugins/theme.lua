a3087abe2dfe02dd1e6a91518b11d9fb3674fcd3
Daniel Czerwonk <czerwonk@mauve.de>
2023-04-26 19:00:27 +0200

    tweak color selection of kanagawa

--- home/programs/vim/plugins/theme.lua
+++ home/programs/vim/plugins/theme.lua
@@ -1,1 +1,15 @@
-vim.cmd("colorscheme kanagawa-wave")
+require('kanagawa').setup {
+  compile = true,
+  transparent = true,
+  theme = 'wave',
+  colors = {
+    palette = {
+      sumiInk1 = "#1E1E1E",
+      sumiInk4 = "#222222",
+      springViolet1 = "#658594",
+      oniViolet = "#A3D4D5",
+      crystalBlue = "#569CD6",
+    },
+  },
+}
+vim.cmd("colorscheme kanagawa")
