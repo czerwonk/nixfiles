1a8f73bc2452c7df68981f7aaa148e04bbb921af
Daniel Czerwonk <czerwonk@mauve.de>
2023-04-28 17:16:45 +0200

    small color optimizations for kitty

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -11,6 +11,10 @@
     settings = {
       enable_audio_bell = false;
       hide_window_decorations = true;
+      color2 = "#98BB6C";
+      color7 = "#E6C384";
+      color15 = "#DCD7BA";
+      color31 = "#7FB4CA";
     };
   };
 }
