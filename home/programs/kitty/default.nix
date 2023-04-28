77bf5b42d8d1b370d5394994b559e7296f060502
Daniel Czerwonk <czerwonk@mauve.de>
2023-04-28 17:50:37 +0200

    kitty yellow tuning

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -12,10 +12,10 @@
       enable_audio_bell = false;
       hide_window_decorations = true;
       foreground = "#E1D9D1";
-      color2 = "#98BB6C";
-      color11 = "#E6C384";
+      color2 = "#76946A";
+      color3 = "#E6C384";
       color31 = "#7FB4CA";
-      color76 = "#76946A";
+      color76 = "#98BB6C";
     };
   };
 }
