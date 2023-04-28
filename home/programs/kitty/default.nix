26feb123d7c84cd2e74e27583ab026802327c964
Daniel Czerwonk <czerwonk@mauve.de>
2023-04-28 18:00:28 +0200

    kitty color tweak

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -12,9 +12,12 @@
       enable_audio_bell = false;
       hide_window_decorations = true;
       foreground = "#E1D9D1";
+      color1 = "#C34043";
       color2 = "#76946A";
       color3 = "#E6C384";
+      color4 = "#658594";
       color31 = "#7FB4CA";
+      color75 = "#FF9E3B";
       color76 = "#98BB6C";
     };
   };
