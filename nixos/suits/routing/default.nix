5154185b7f953ea80dc18286867b9bde10598f3b
Daniel Czerwonk <czerwonk@mauve.de>
2023-05-19 17:20:13 +0200

    run routinator in container

--- nixos/suits/routing/default.nix
+++ nixos/suits/routing/default.nix
@@ -10,4 +10,13 @@
     config = "include /etc/bird/routing-rocks.conf;";
     checkConfig = false;
   };
+
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.oci-containers.containers = {
+    routinator = {
+      image = "nlnetlabs/routinator";
+      autoStart = true;
+      ports = [ "127.0.0.1:3323:3323" ];
+    };
+  };
 }
