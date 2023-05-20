50269df027d2d55aebe9b911072bacadfd74981b
Daniel Czerwonk <czerwonk@mauve.de>
2023-05-20 10:01:39 +0200

    remove redundant definitions

--- nixos/suits/routing/sysctl.nix
+++ nixos/suits/routing/sysctl.nix
@@ -7,8 +7,6 @@
     "net.ipv4.conf.all.forwarding" = true;
     "net.ipv4.conf.default.forwarding" = true;
     "net.ipv4.tcp_low_latency" = true;
-    "net.ipv4.tcp_timestamps" = false;
-    "net.ipv4.tcp_syncookies" = true;
     "net.ipv4.route.max_size" = 2147483647;
 
     "net.ipv6.conf.all.forwarding" = true;
