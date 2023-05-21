a46450f2de4a01063156aecff62b8de96a9ae899
Daniel Czerwonk <daniel@routing.rocks>
2023-05-21 17:33:43 +0200

    move IP configuration

--- nixos/hosts/bb1/configuration.nix
+++ nixos/hosts/bb1/configuration.nix
@@ -8,22 +8,4 @@
     ../../suits/container
     ../../suits/routing
   ];
-
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-    useOSProber = true;
-  };
-
-  networking = {
-    domain = "dus.routing.rocks";
-    dhcpcd.enable = false;
-    interfaces = {
-      enp0s5.ipv4.addresses = [{
-        address = "10.211.55.128";
-        prefixLength = 24;
-      }];
-    };
-    defaultGateway = "10.211.55.1";
-  };
 }
