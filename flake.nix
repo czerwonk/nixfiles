5d5c3de1a47d760d1f104453363282e83c5ded8e
Daniel Czerwonk <daniel@routing.rocks>
2023-05-29 09:36:58 +0200

    switch from unstable to 23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Daniel Czerwonk's Nix configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
     home-manager = {
       url = "github:nix-community/home-manager";
