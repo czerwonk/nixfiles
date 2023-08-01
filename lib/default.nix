9b6148908b535fe5f309e5f1044301acfbff5854
Daniel Czerwonk <daniel@routing.rocks>
2023-08-01 16:59:04 +0200

    remove unneccessary rec usage

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,6 @@
 { home-manager, nixpkgs, nixpkgs-unstable, ... }:
 
-rec {
+{
   userUtil = import ./user.nix { inherit home-manager nixpkgs nixpkgs-unstable; };
   systemUtil = import ./system.nix { inherit home-manager nixpkgs nixpkgs-unstable; };
 }
