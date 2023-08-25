8fb02d13951d6c6aa7ce9793409ae1018d9a39c0
Daniel Czerwonk <daniel@routing.rocks>
2023-08-25 11:12:55 +0200

    Revert "add ansible-role alias"
    This reverts commit 37fb665dee885fe4796bdcdbea0c1129e2b5b8fe.

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -5,7 +5,6 @@
       git-worktree-it = "bash ~/.scripts/git-worktree-it.sh";
       git-worktree-clone = "bash ~/.scripts/git-worktree-clone.sh";
       vim = "bash ~/.scripts/nvim-tmux.sh";
-      ansible-role = "bash ~/.scripts/ansible-role.sh";
     };
     file.".scripts" = {
       source = ./files;
--- home/scripts/files/ansible-role.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-role_name=$1
-shift
-
-ansible -m include_role -a "name=$role_name" $@
