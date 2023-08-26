368af40c1c11d2da64d79f84f2e5fce5df0b53eb
Daniel Czerwonk <daniel@routing.rocks>
2023-08-26 10:20:10 +0200

    fix completion shortcut

--- home/programs/vim/plugins/cmp.lua
+++ home/programs/vim/plugins/cmp.lua
@@ -54,7 +54,7 @@ cmp.setup {
     ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
     ["<CR>"] = cmp.mapping.confirm { select = false },
     ["<S-CR>"] = cmp.mapping.confirm {
