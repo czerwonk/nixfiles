45497d339e2608e9acca66ed358b3bfd8a373afe
Daniel Czerwonk <daniel@routing.rocks>
2023-09-05 19:47:07 +0200

    inprove format on save

--- home/programs/vim/plugins/lsp.lua
+++ home/programs/vim/plugins/lsp.lua
@@ -220,6 +220,6 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
   callback = function()
     vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
-    vim.lsp.buf.format { async = true }
+    vim.lsp.buf.format({ async = false })
   end
 })
