dee48781b2ed659e373f0810bc045b4f0d6ba6ed
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2023-09-05 22:31:50 +0200

    Update cmp.lua

--- home/programs/vim/plugins/cmp.lua
+++ home/programs/vim/plugins/cmp.lua
@@ -56,10 +56,7 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
-    ["<CR>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    },
+    ["<CR>"] = cmp.mapping.confirm { select = true },
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
