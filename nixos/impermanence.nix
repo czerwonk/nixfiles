b831f3a23407980740f4068d2800cc7b5f021c22
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2023-09-14 07:30:05 +0200

    Update impermanence.nix

--- nixos/impermanence.nix
+++ nixos/impermanence.nix
@@ -23,10 +23,6 @@
       "/data"
     ];
     files = [
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
       "/etc/machine-id"
       "/root/.ssh/known_hosts"
     ];
