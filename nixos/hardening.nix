dc176fcc57566b16dd609f8a421de7bc54f81490
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2023-09-14 11:31:41 +0200

    Update hardening.nix

--- nixos/hardening.nix
+++ nixos/hardening.nix
@@ -10,7 +10,7 @@
 
   security.protectKernelImage = true;
   security.forcePageTableIsolation = true;
-  security.lockKernelModules = true;
+  security.lockKernelModules = lib.mkDefault true;
   security.sudo.execWheelOnly = true;
   security.allowUserNamespaces = lib.mkDefault false;
 
