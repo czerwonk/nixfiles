0c3f2dbcd26abe5296ae12d8e92b50e79458ff5f
Daniel Czerwonk <daniel@routing.rocks>
2023-10-12 18:10:36 +0200

    use ibl 3.0 config

--- home/programs/vim/plugins/indent-blankline.lua
+++ home/programs/vim/plugins/indent-blankline.lua
@@ -3,27 +3,4 @@ vim.opt.listchars:append({
   trail = '⋅',
   tab = '⇢ ',
 })
-require('indent_blankline').setup {
-  char = '│',
-  space_char_blankline = ' ',
-  show_first_indent_level = true,
-  show_trailing_blankline_indent = false,
-  filetype_exclude = {
-    'lspinfo',
-    'checkhealth',
-    'help',
-    'man',
-    'git',
-    'markdown',
-    'text',
-    'terminal',
-    'TelescopePrompt',
-    'TelescopeResults',
-  },
-  buftype_exclude = {
-    'terminal',
-    'nofile',
-    'quickfix',
-    'prompt',
-  },
-}
+require('ibl').setup()
