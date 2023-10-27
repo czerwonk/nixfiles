6d9e793c060958ffe6f8767ca3ac1ac8ac72a1fa
Daniel Czerwonk <daniel@routing.rocks>
2023-10-27 15:35:53 +0200

    add systemd services for grive2

--- home/programs/grive2/default.nix
+++ home/programs/grive2/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   home = {
@@ -6,5 +6,55 @@
       grive2
       inotify-tools
     ];
+    file.".scripts/grive2/grive-sync.sh".source = ./grive-sync.sh;
+    file.".config/systemd/user/grive@.service".text = ''
+      [Unit]
+      Description=Google drive sync (main)
+      Requires=grive-timer@%i.timer grive-changes@%i.service
+
+      # dummy service
+      [Service]
+      Type=oneshot
+      ExecStart=${pkgs.coreutils}/bin/true
+      # This service shall be considered active after start
+      RemainAfterExit=yes
+
+      [Install]
+      WantedBy=default.target
+    '';
+    file.".config/systemd/user/grive-timer@.timer".text = ''
+      [Unit]
+      Description=Google drive sync (fixed intervals)
+
+      [Timer]
+      OnCalendar=*:0/5
+      OnBootSec=3min
+      OnUnitActiveSec=5min
+      Unit=grive-timer@%i.service
+
+      [Install]
+      WantedBy=timers.target
+    '';
+    file.".config/systemd/user/grive-changes@.service".text = ''
+      [Unit]
+      Description=Google drive sync (changed files)
+
+      [Service]
+      ExecStart=${config.home.homeDirectory}/.scripts/grive2/grive-sync.sh listen "%i"
+      Type=simple
+      Restart=always
+      RestartSec=30
+
+      [Install]
+      WantedBy=default.target
+    '';
+    file.".config/systemd/user/grive-timer@.service".text = ''
+      [Unit]
+      Description=Google drive sync (executed by timer unit)
+      After=network-online.target
+
+      [Service]
+      ExecStart=${config.home.homeDirectory}/.scripts/grive2/grive-sync.sh sync "%i"
+    '';
   };
 }
