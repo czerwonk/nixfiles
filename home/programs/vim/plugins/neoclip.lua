3e8f53b196ce8884a49592a21756be52d79a0496
Daniel Czerwonk <daniel@routing.rocks>
2023-11-02 08:05:46 +0100

    tune neoclip

--- home/programs/vim/plugins/neoclip.lua
+++ home/programs/vim/plugins/neoclip.lua
@@ -1,4 +1,21 @@
-require('neoclip').setup()
-require('telescope').load_extension('neoclip')
+local function is_whitespace(line)
+  return vim.fn.match(line, [[^\s*$]]) ~= -1
+end
 
+local function all(tbl, check)
+  for _, entry in ipairs(tbl) do
+    if not check(entry) then
+      return false
+    end
+  end
+  return true
+end
+
+require('neoclip').setup {
+  filter = function(data)
+    return not all(data.event.regcontents, is_whitespace)
+  end
+}
+
+require('telescope').load_extension('neoclip')
 vim.keymap.set('n', '<leader>o', '<cmd>Telescope neoclip<cr>', { desc = 'Neoclip' })
