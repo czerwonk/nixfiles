742e12433ea6c4fc1627ce3eca5fe3a0ad1047ab
Daniel Czerwonk <daniel@routing.rocks>
2023-11-02 06:37:42 +0100

    cleanup

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -7,21 +7,5 @@
       HostKeyAlgorithms=+ssh-rsa
       PubkeyAcceptedAlgorithms=+ssh-rsa
     '';
-    matchBlocks = {
-      routing-rocks = {
-        host = "*.routing.rocks";
-        user = "daniel";
-        port = 2222;
-      };
-      ffe_backbone = {
-        host = "bb-*.ff-e.net";
-        user = "dan_nrw";
-        port = 2222;
-      };
-      ffe = {
-        host = "*.freifunk-essen.net *.ff-e.net";
-        user = "dan_nrw";
-      };
-    };
   };
 }
