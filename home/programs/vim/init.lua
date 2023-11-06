68452ca0a76c5001660458ca4e602cfe97df91cf
Daniel Czerwonk <daniel@routing.rocks>
2023-11-06 13:38:28 +0100

    add keybinding for hex view

--- home/programs/vim/init.lua
+++ home/programs/vim/init.lua
@@ -25,39 +25,43 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
 vim.opt.autoread = true
-vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'CursorHoldI', 'FocusGained' }, {
   command = "if mode() != 'c' | checktime | endif",
-  pattern = { "*" },
+  pattern = { '*' },
 })
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 
 -- convenience key mappings
 vim.keymap.set('n', 'Y', 'Y', { desc = 'Yank line' }) -- restore yank behavior of VIM
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down' }) -- reposition after scrolling down
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up' }) -- reposition after scrolling up
-vim.keymap.set("n", "n", "nzzzv") -- reposition after junp to match
-vim.keymap.set("n", "N", "Nzzzv") -- reposition after junp to match
-vim.keymap.set("n", "Q", "<nop>") -- disable Q
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = 'Yank to Clipbaord' })
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = 'Yank to Clipbaord' })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move down' })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move up'})
-vim.keymap.set("n", "<leader>cc", "<cmd>copen<CR>", { desc = 'Open quickfix list' })
-vim.keymap.set("n", "<leader>cn", "<cmd>cnext<CR>", { desc = 'Next quickfix list item' })
-vim.keymap.set("n", "<leader>cp", "<cmd>cprev<CR>", { desc = 'Previous quickfix list item' })
-vim.keymap.set("n", "<leader>b", "<cmd>bnext<CR>", { desc = 'Switch to next buffer' })
-vim.keymap.set("n", "<leader>w", function() require("mini.bufremove").delete(0, false) end, { desc = 'Close current buffer' })
+vim.keymap.set('n', 'n', 'nzzzv') -- reposition after junp to match
+vim.keymap.set('n', 'N', 'Nzzzv') -- reposition after junp to match
+vim.keymap.set('n', 'Q', '<nop>') -- disable Q
+vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]], { desc = 'Yank to Clipbaord' })
+vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Yank to Clipbaord' })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move up'})
+vim.keymap.set('n', '<leader>cc', '<cmd>copen<CR>', { desc = 'Open quickfix list' })
+vim.keymap.set('n', '<leader>cn', '<cmd>cnext<CR>', { desc = 'Next quickfix list item' })
+vim.keymap.set('n', '<leader>cp', '<cmd>cprev<CR>', { desc = 'Previous quickfix list item' })
+vim.keymap.set('n', '<leader>b', '<cmd>bnext<CR>', { desc = 'Switch to next buffer' })
+vim.keymap.set('n', '<leader>w', function() require('mini.bufremove').delete(0, false) end, { desc = 'Close current buffer' })
+vim.keymap.set('n', '<leader>H', function()
+  vim.cmd('%!xxd')
+  vim.bo[0].ft = 'xxd'
+end, { desc = 'Hex view' })
 
 -- icons & signs
 local signs = {
-  Error = " ", Warn = " ", Hint = " ", Info = " "
+  Error = ' ', Warn = ' ', Hint = ' ', Info = ' '
 }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
+  local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, {
     text = icon, texthl = hl, numhl = hl
   })
