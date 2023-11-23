f7d68cc81c2759916bbf26c2ed3b942a72338a99
Daniel Czerwonk <daniel@routing.rocks>
2023-11-23 17:02:25 +0100

    try to improve fprintd experience

--- nixos/suits/desktop/default.nix
+++ nixos/suits/desktop/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, username, ... }:
+{ pkgs, lib, config, username, ... }:
 
 {
   imports = [
@@ -34,4 +34,25 @@
     enableSSHSupport = true;
   };
   services.pcscd.enable = true;
+
+  security.pam.services.login.fprintAuth = false;
+  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+    text = ''
+      auth       required                    pam_shells.so
+      auth       requisite                   pam_nologin.so
+      auth       requisite                   pam_faillock.so      preauth
+      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      auth       optional                    pam_permit.so
+      auth       required                    pam_env.so
+      auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
+      auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+      account    include                     login
+
+      password   required                    pam_deny.so
+
+      session    include                     login
+      session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+    '';
+  };
 }
