2a3f547ebd5d42215ce7253d7c916b766609dab7
Daniel Czerwonk <daniel@routing.rocks>
2023-11-24 06:42:07 +0100

    nix flake update

--- home/programs/vim/default.nix
+++ home/programs/vim/default.nix
@@ -222,14 +222,14 @@
       {
         plugin = nvim-dap-go;
       }
-      # {
-      #   plugin = neotest;
-      #   type = "lua";
-      #   config = builtins.readFile ./plugins/test.lua;
-      # }
-      # {
-      #   plugin = neotest-go;
-      # }
+      {
+        plugin = neotest;
+        type = "lua";
+        config = builtins.readFile ./plugins/test.lua;
+      }
+      {
+        plugin = neotest-go;
+      }
       {
         plugin = trouble-nvim;
         type = "lua";
