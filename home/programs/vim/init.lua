2b44b568faaab882e9965d7e18b8e66fc570e0d5
Daniel Czerwonk <daniel@routing.rocks>
2023-12-11 13:08:23 +0100

    add remapping for macros

--- home/programs/vim/init.lua
+++ home/programs/vim/init.lua
@@ -43,7 +43,8 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down' }) -- reposition 
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up' }) -- reposition after scrolling up
 vim.keymap.set('n', 'n', 'nzzzv') -- reposition after junp to match
 vim.keymap.set('n', 'N', 'Nzzzv') -- reposition after junp to match
-vim.keymap.set('n', 'Q', '<nop>') -- disable Q
+vim.keymap.set('n', 'Q', '@qj', { desc = 'Apply Macro' })
+vim.keymap.set('x', 'Q', ':norm @q<CR>', { desc = 'Apply Macro' })
 vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]], { desc = 'Yank to Clipbaord' })
 vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Yank to Clipbaord' })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move down' })
