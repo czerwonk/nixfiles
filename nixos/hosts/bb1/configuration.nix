49e01a379345edeb81e690a943eb489d9277c5da
Daniel Czerwonk <daniel@routing.rocks>
2023-12-12 16:14:23 +0100

    configure impemanence for bb1

--- nixos/hosts/bb1/configuration.nix
+++ nixos/hosts/bb1/configuration.nix
@@ -1,3 +1,5 @@
+{ lib, ... }:
+
 {
   imports = [ 
     ./hardware-configuration.nix
@@ -9,4 +11,19 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.postDeviceCommands = lib.mkBefore ''
+    mkdir -p /btrfs_mnt
+    mount -o subvol=/ /dev/disk/by-uuid/96ea7ea2-62e9-47bd-a935-4b1bcdfc2229 /btrfs_mnt
+    echo "Delete old root subvolume..."
+    btrfs subvolume list -o /btrfs_mnt/root |
+      cut -f 9 -d ' ' |
+      while read subvolume; do
+        echo "Delete subvolume $subvolume..."
+        btrfs subvolume delete "/btrfs_mnt/$subvolume"
+      done
+    btrfs subvolume delete /btrfs_mnt/root
+    echo "Create new root subvolume..."
+    btrfs subvolume create /btrfs_mnt/root
+    umount /btrfs_mnt
+  '';
 }
