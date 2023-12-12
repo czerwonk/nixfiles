1d673fb967db61d1a5f884d173ff0535aa5e3da7
Daniel Czerwonk <daniel@routing.rocks>
2023-12-12 17:05:30 +0100

    fix persistence for wireguard keys

--- nixos/suits/server/impermanence.nix
+++ nixos/suits/server/impermanence.nix
@@ -1,11 +1,13 @@
 {
   environment.persistence."/persist" = {
+    directories = [
+      "/etc/wireguard"
+    ];
     files = [
       "/etc/ssh/ssh_host_rsa_key"
       "/etc/ssh/ssh_host_rsa_key.pub"
       "/etc/ssh/ssh_host_ed25519_key"
       "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/wireguard"
     ];
   };
 }
