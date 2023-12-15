4c0b5b41ff2a24dfb1639302bbce5907da13a724
Daniel Czerwonk <daniel@routing.rocks>
2023-12-15 11:26:00 +0100

    add ipv6 addresses for DNS queries

--- nixos/dns.nix
+++ nixos/dns.nix
@@ -11,21 +11,40 @@
       listen_addresses = [ "127.0.0.1" "0::1" ];
       dnssec_return_status = "GETDNS_EXTENSION_TRUE";
       resolution_type = "GETDNS_RESOLUTION_STUB";
-      upstream_recursive_servers = [{
-        address_data = "1.1.1.1";
-        tls_auth_name = "cloudflare-dns.com";
-        tls_pubkey_pinset = [{
-          digest = "sha256";
-          value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
-        }];
-      } {
-        address_data = "1.0.0.1";
-        tls_auth_name = "cloudflare-dns.com";
-        tls_pubkey_pinset = [{
-          digest = "sha256";
-          value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
-        }];
-      }];
+      upstream_recursive_servers = [
+        {
+          address_data = "1.1.1.1";
+          tls_auth_name = "one.one.one.one";
+          tls_pubkey_pinset = [{
+            digest = "sha256";
+            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+          }];
+        } 
+        {
+          address_data = "1.0.0.1";
+          tls_auth_name = "one.one.one.one";
+          tls_pubkey_pinset = [{
+            digest = "sha256";
+            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+          }];
+        }
+        {
+          address_data = "2606:4700:4700::1111";
+          tls_auth_name = "one.one.one.one";
+          tls_pubkey_pinset = [{
+            digest = "sha256";
+            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+          }];
+        }
+        {
+          address_data = "2606:4700:4700::1001";
+          tls_auth_name = "one.one.one.one";
+          tls_pubkey_pinset = [{
+            digest = "sha256";
+            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+          }];
+        }
+      ];
     };
   };
 }
