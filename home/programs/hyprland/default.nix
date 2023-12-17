0f3b45d195fd46d668b4f4ecb2bed1dd04f59af7
Daniel Czerwonk <daniel@routing.rocks>
2023-12-17 17:01:28 +0100

    start kitty non-silent

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -46,7 +46,7 @@ in {
       exec-once = ${pkgs.waybar}/bin/waybar
       exec-once = ${pkgs.wlsunset}/bin/wlsunset -l -23 -L -46
       exec-once = ${pkgs.hyprpaper}/bin/hyprpaper
-      exec-once = [workspace 2 silent] ${pkgs.kitty}/bin/kitty ${pkgs.tmux}/bin/tmux a
+      exec-once = [workspace 2] ${pkgs.kitty}/bin/kitty ${pkgs.tmux}/bin/tmux a
       exec-once = [workspace 3 silent] ${pkgs.brave}/bin/brave
       exec-once = [workspace 4 silent] ${pkgs.thunderbird}/bin/thunderbird
 
