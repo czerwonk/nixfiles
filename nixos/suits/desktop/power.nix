0b853f36234862b3344218d59ccdc1523654f6dd
Daniel Czerwonk <daniel@routing.rocks>
2023-12-18 08:09:25 +0100

    optimize power management

--- nixos/suits/desktop/power.nix
+++ nixos/suits/desktop/power.nix
@@ -1,9 +1,23 @@
 { lib, ... }:
 
 {
-  powerManagement = {
+  boot.kernelParams = [ "mem_sleep_default=deep" ];
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+
+  services.tlp = {
     enable = true;
-    powertop.enable = true;
-    cpuFreqGovernor = lib.mkDefault "ondemand";
+    settings = {
+      CPU_BOOST_ON_BAT = 0;
+
+      START_CHARGE_THRESH_BAT0 = 90;
+      STOP_CHARGE_THRESH_BAT0 = 97;
+
+      RUNTIME_PM_ON_BAT = "auto";
+
+      CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
+    };
   };
+
+  services.power-profiles-daemon.enable = false; # conflicts with tlp
 }
