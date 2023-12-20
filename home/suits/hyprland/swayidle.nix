bb3e38cefd7a5dc5b57918b3a72caab1d144f352
Daniel Czerwonk <daniel@routing.rocks>
2023-12-20 10:40:45 +0100

    turn off monitors after 15 minutes

--- home/suits/hyprland/swayidle.nix
+++ home/suits/hyprland/swayidle.nix
@@ -8,7 +8,8 @@
       { event = "lock"; command = "${config.programs.swaylock.package}/bin/swaylock -fF"; }
     ];
     timeouts = [
-      { timeout = 900; command = "${config.programs.swaylock.package}/bin/swaylock -fF"; }
+      { timeout = 300; command = "${config.programs.swaylock.package}/bin/swaylock -fF"; }
+      { timeout = 900; command = "hyprctl dispatch dpms off"; resumeCommand = "hyprctl dispatch dpms on"; }
       { timeout = 1800; command = "${pkgs.systemd}/bin/systemctl suspend"; }
     ];
   };
