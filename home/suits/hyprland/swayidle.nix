0a9141204dc9650a03f0154ff092ab6fa04e7497
Daniel Czerwonk <daniel@routing.rocks>
2023-12-20 07:40:24 +0100

    configure lock event in swayidle

--- home/suits/hyprland/swayidle.nix
+++ home/suits/hyprland/swayidle.nix
@@ -5,11 +5,11 @@
     enable = true;
     events = [
       { event = "before-sleep"; command = "${config.programs.swaylock.package}/bin/swaylock -fF"; }
-      { event = "lock"; command = "lock"; }
+      { event = "lock"; command = "${config.programs.swaylock.package}/bin/swaylock -fF"; }
     ];
     timeouts = [
-      { timeout = 300; command = "${config.programs.swaylock.package}/bin/swaylock -fF"; }
-      { timeout = 900; command = "${pkgs.systemd}/bin/systemctl suspend"; }
+      { timeout = 900; command = "${config.programs.swaylock.package}/bin/swaylock -fF"; }
+      { timeout = 1800; command = "${pkgs.systemd}/bin/systemctl suspend"; }
     ];
   };
 }
