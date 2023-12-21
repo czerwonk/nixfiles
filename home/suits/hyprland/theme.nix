b82dbe9e782ec216284368223aef81b67fe408da
Daniel Czerwonk <daniel@routing.rocks>
2023-12-21 12:40:08 +0100

    fix GTK settings

--- home/suits/hyprland/theme.nix
+++ home/suits/hyprland/theme.nix
@@ -11,14 +11,10 @@
   gtk = {
     enable = true;
     gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
     };
     gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
     };
   };
 
