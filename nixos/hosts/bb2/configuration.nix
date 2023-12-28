fa4ba2d72619b89ca2a330f4ad928c0ebb5ce3a8
Daniel Czerwonk <daniel@routing.rocks>
2023-12-28 10:44:21 +0100

    configure purging of root on boot for bb2

--- nixos/hosts/bb2/configuration.nix
+++ nixos/hosts/bb2/configuration.nix
@@ -1,3 +1,5 @@
+{ lib, ... }:
+
 {
   imports = [ 
     ./hardware-configuration.nix
@@ -9,4 +11,19 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+    mkdir -p /btrfs_mnt
+    mount -o subvol=/ /dev/disk/by-uuid/2cb2ec75-f31f-4677-a5ab-7bbd5c2a1439 /btrfs_mnt
+    echo "Delete old root subvolume..."
+    btrfs subvolume list -o /btrfs_mnt/root |
+      cut -f 9 -d ' ' |
+      while read subvolume; do
+        echo "Delete subvolume $subvolume..."
+        btrfs subvolume delete "/btrfs_mnt/$subvolume"
+      done
+    btrfs subvolume delete /btrfs_mnt/root
+    echo "Create new root subvolume..."
+    btrfs subvolume create /btrfs_mnt/root
+    umount /btrfs_mnt
+  '';
 }
