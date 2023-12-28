dd559ee2ab67a15a4591eab89dcf482bf374204b
Daniel Czerwonk <daniel@routing.rocks>
2023-12-28 09:50:22 +0100

    disable docker compat, install docker client to communicate with podman

--- nixos/suits/container/default.nix
+++ nixos/suits/container/default.nix
@@ -1,11 +1,18 @@
+{ pkgs, ... }:
+
 {
   networking.firewall.trustedInterfaces = [ "podman+" ];
+
   virtualisation = {
     podman = {
       enable = true;
-      dockerCompat = true;
       dockerSocket.enable = true;
       defaultNetwork.settings.dns_enabled = true;
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    docker-client
+    arion
+  ];
 }
