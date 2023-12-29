522370b4ed92488a7eacfc28211d137c7f2f217c
Daniel Czerwonk <daniel@routing.rocks>
2023-12-29 09:40:52 +0100

    simplify nc

--- nixos/services/nextcloud/default.nix
+++ nixos/services/nextcloud/default.nix
@@ -17,16 +17,12 @@ in {
       nextcloud-aio-mastercontainer = {
         image = "nextcloud/all-in-one";
         autoStart = true;
-        ports = [ "127.0.0.1:11000:11000" ];
+        ports = [ "127.0.0.1:8080:8080" ];
         volumes = [
           "nextcloud_aio_mastercontainer:/mnt/docker-aio-config"
           "nextcloud_data:/mnt/ncdata"
           "/var/run/docker.sock:/var/run/docker.sock:ro"
         ];
-        environment = {
-          APACHE_PORT = "11000";
-          APACHE_IP_BINDING = "0.0.0.0";
-        };
       };
     };
   };
