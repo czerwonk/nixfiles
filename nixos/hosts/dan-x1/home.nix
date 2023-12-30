ccc193c5d8289517dbfc4ceef23434cec522a05d
Daniel Czerwonk <daniel@routing.rocks>
2023-12-30 07:00:32 +0100

    disable grive

--- nixos/hosts/dan-x1/home.nix
+++ nixos/hosts/dan-x1/home.nix
@@ -6,11 +6,8 @@
     ../../../home/suits/desktop
     ../../../home/suits/devops
     ../../../home/suits/pentest
-    ../../../home/services/grive2
     ../../../home/suits/hyprland
   ] ++ extraHomeModules;
-
-  services.grive2.enable = true;
 
   suits.hyprland = {
     backlightDevice = "intel_backlight";
