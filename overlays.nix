ba014e5a6a170c309448ad471ae36383edc66ee4
Daniel Czerwonk <daniel@routing.rocks>
2024-01-09 09:09:55 +0100

    use brave from unstable

--- overlays.nix
+++ overlays.nix
@@ -20,6 +20,7 @@ in {
       podman = pkgs-unstable.podman;
       podman-unwrapped = pkgs-unstable.podman-unwrapped;
       sublime4 = pkgs-unstable.sublime4;
+      brave = pkgs-unstable.brave;
       vimPlugins = pkgs-unstable.vimPlugins;
       nix-alien = nix-alien-packages.nix-alien;
       kanagawa-gtk-theme = pkgs.callPackage ./pkgs/kanagawa-gtk-theme {};
