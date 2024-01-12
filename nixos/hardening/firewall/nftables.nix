aef81a42bd566e2f4973654b87085b7a803a0f0b
Daniel Czerwonk <daniel@routing.rocks>
2024-01-12 11:03:54 +0100

    fix typo

--- nixos/hardening/firewall/nftables.nix
+++ nixos/hardening/firewall/nftables.nix
@@ -2,7 +2,7 @@
 
 {
   config = lib.mkIf config.networking.nftables.enable {
-    networking.nftables.tables."nixos-fw".content = lib.mkafter ''
+    networking.nftables.tables."nixos-fw".content = lib.mkAfter ''
           chain log-and-drop {
             tcp flags syn / fin,syn,rst,ack log prefix "refused connection: " level info
             drop
