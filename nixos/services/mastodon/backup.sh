6822040f80c356b2379fc81979477f1e8d07a869
Daniel Czerwonk <daniel@routing.rocks>
2024-01-15 07:29:17 +0100

    backup purging

--- nixos/services/mastodon/backup.sh
+++ nixos/services/mastodon/backup.sh
@@ -1,6 +1,8 @@
 set -e
 
-BACKUP_DIR="/data/backup/mastodon/$(date +%Y-%m-%d)"
+BACKUP_DIR_BASE="/data/backup/matrix"
+BACKUP_DIR="$BACKUP_DIR_BASE/$(date +%Y-%m-%d)"
+
 mkdir -p $BACKUP_DIR
 
 pushd /opt/mastodon
@@ -18,3 +20,6 @@ tar cfz $BACKUP_DIR/redis.tgz /var/lib/containers/storage/volumes/mastodon_redis
 
 echo "Backup files"
 tar cfz $BACKUP_DIR/files.tgz /var/lib/containers/storage/volumes/mastodon_system-data/_data/
+
+echo "Purge old backups"
+find $BACKUP_DIR_BASE -maxdepth 1 -type d -mtime +2 -exec rm -R {} \;
--- nixos/services/matrix/backup.sh
+++ nixos/services/matrix/backup.sh
@@ -1,6 +1,8 @@
 set -e
 
-BACKUP_DIR="/data/backup/matrix/$(date +%Y-%m-%d)"
+BACKUP_DIR_BASE="/data/backup/matrix"
+BACKUP_DIR="$BACKUP_DIR_BASE/$(date +%Y-%m-%d)"
+
 mkdir -p $BACKUP_DIR
 
 echo "Backup files"
@@ -10,3 +12,6 @@ echo "Backup DB"
 pushd /opt/matrix
 podman-compose exec -u postgres db pg_dumpall --user synapse | gzip > $BACKUP_DIR/db.sql.gz
 popd
+
+echo "Purge old backups"
+find $BACKUP_DIR_BASE -maxdepth 1 -type d -mtime +2 -exec rm -R {} \;
