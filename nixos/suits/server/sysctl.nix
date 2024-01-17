73adc09ab4920656611f4de5b373942302d020aa
Daniel Czerwonk <daniel@routing.rocks>
2024-01-17 07:31:50 +0100

    reduce swappiness on servers

--- nixos/suits/server/sysctl.nix
+++ nixos/suits/server/sysctl.nix
@@ -1,5 +1,6 @@
 {
   boot.kernel.sysctl = {
+    "vm.swappiness" = 1;
     "net.core.default_qdisc" = "fq";
     "net.ipv4.tcp_congestion_control" = "bbr";
     "net.ipv4.tcp_notsent_lowat" = 16384;
