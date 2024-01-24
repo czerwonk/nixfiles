75731fe49a1ae41fbdd842a12704839b761970cd
Daniel Czerwonk <daniel@routing.rocks>
2024-01-24 17:48:54 +0100

    migrate ripe-atlas to oci-container

--- nixos/services/ripe-atlas/default.nix
+++ nixos/services/ripe-atlas/default.nix
@@ -4,6 +4,10 @@ with lib;
 
 let
   cfg = config.services.custom.ripe-atlas;
+  regservers = pkgs.writeScriptBin "reg_servers.sh" ''
+    REG_1_HOST=2001:67c:2e8:11::c100:13f6
+    REG_2_HOST=2001:67c:2e8:11::c100:13f7
+  '';
 
 in {
   options = {
@@ -13,19 +17,35 @@ in {
   };
 
   config = mkIf cfg.enable {
-    systemd.services.ripe-atlas-probe = {
-      description = "Atlas Software Probe";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
+    systemd.services.podman-create-ripe-atlas-net = {
+      serviceConfig.Type = "oneshot";
+      wantedBy = [ "podman-ripe-atlas.service" ];
       path = [ pkgs.podman ];
-      serviceConfig = {
-        WorkingDirectory = "/opt/docker-ripe-atlas";
-        Type = "simple";
-        ExecStart = "${pkgs.podman-compose}/bin/podman-compose up -d";
-        ExecStop = "${pkgs.podman-compose}/bin/podman-compose down";
-        RemainAfterExit = true;
-        Restart = "always";
-        RestartSec = 60;
+      script = ''
+        podman network exists ripe-atlas || podman network create -n ripe-atlas --ipv6 --subnet=2001:678:1e0:f200::/64 --gateway=2001:678:1e0:f200::1
+      '';
+    };
+
+    virtualisation.oci-containers.containers = {
+      ripe-atlas = {
+        autoStart = true;
+        extraOptions = [
+          "--network=ripe-atlas"
+          "--cap-drop=all"
+          "--cap-add=CHOWN,SETUID,SETGID,DAC_OVERRIDE,NET_RAW"
+        ];
+
+        image = "jamesits/ripe-atlas";
+
+        environment = {
+          RXTXRPT = "yes";
+        };
+
+        volumes = [
+          "docker-ripe-atlas_etc:/var/atlas-probe/etc"
+          "docker-ripe-atlas_status:/var/atlas-probe/status"
+          "${regservers}/bin/reg_servers.sh:/var/atlas-probe/bin/reg_servers.sh"
+        ];
       };
     };
   };
