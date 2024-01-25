4c313573912b7872a2f0f3f39872e88562dbf728
Daniel Czerwonk <daniel@routing.rocks>
2024-01-25 09:15:59 +0100

    migrate nextcloud to oci containers

--- nixos/services/nextcloud/default.nix
+++ nixos/services/nextcloud/default.nix
@@ -3,29 +3,276 @@
 with lib;
 
 let
+  nextcloudDomain = "nextcloud.routing.rocks";
+  timezone = "Europe/Berlin";
   cfg = config.services.custom.nextcloud;
 
 in {
   options = {
     services.custom.nextcloud = {
       enable = mkEnableOption "Nextcloud (All In One)";
+
+      databasePassword = mkOption {
+        type = types.str;
+        description = "Password for the PostgreSQL database";
+      };
+
+      redisPassword = mkOption {
+        type = types.str;
+        description = "Password for Redis";
+      };
+
+      nextcloudPassword = mkOption {
+        type = types.str;
+        description = "Password for the admin account";
+      };
+
+      fulltextsearchPassword = mkOption {
+        type = types.str;
+        description = "Password for the full text search";
+      };
     };
   };
 
   config = mkIf cfg.enable {
-    systemd.services.nextcloud = {
-      description = "Nextcloud";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
+    systemd.services.podman-create-nextcloud-net = {
+      serviceConfig.Type = "oneshot";
+      wantedBy = [
+        "podman-nextcloud-aio-apache.service"
+        "podman-nextcloud-aio-database.service"
+        "podman-nextcloud-aio-nextcloud.service"
+        "podman-nextcloud-aio-notify-push.service"
+        "podman-nextcloud-aio-redis.service"
+        "podman-nextcloud-aio-clamav.service"
+        "podman-nextcloud-aio-imaginary.service"
+        "podman-nextcloud-aio-fulltextsearch.service"
+      ];
       path = [ pkgs.podman ];
-      serviceConfig = {
-        WorkingDirectory = "/opt/nextcloud-aio";
-        Type = "simple";
-        ExecStart = "${pkgs.podman-compose}/bin/podman-compose up -d";
-        ExecStop = "${pkgs.podman-compose}/bin/podman-compose down";
-        RemainAfterExit = true;
-        Restart = "always";
-        RestartSec = 60;
+      script = ''
+        podman network exists nextcloud || podman network create nextcloud
+      '';
+    };
+
+    virtualisation.oci-containers.containers = {
+      nextcloud-aio-apache = {
+        autoStart = true;
+        extraOptions = [
+          "--network=nextcloud"
+          "--read-only"
+          "--read-only-tmpfs"
+          "--mount=type=tmpfs,destination=/var/log/supervisord,rw=true"
+          "--mount=type=tmpfs,destination=/var/run/supervisord,rw=true"
+          "--mount=type=tmpfs,destination=/usr/local/apache2/logs,rw=true"
+          "--mount=type=tmpfs,destination=/home/www-data,rw=true"
+        ];
+
+        image = "nextcloud/aio-apache";
+
+        environment = {
+          NC_DOMAIN = "${nextcloudDomain}";
+          NEXTCLOUD_HOST = "nextcloud-aio-nextcloud";
+          NOTIFY_PUSH_HOST = "nextcloud-aio-notify-push";
+          TZ = "${timezone}";
+          APACHE_PORT = "11000";
+          APACHE_MAX_SIZE = "10737418240";
+          APACHE_MAX_TIME = "3600";
+        };
+
+        volumes = [
+          "nextcloud_aio_nextcloud:/var/www/html:ro"
+          "nextcloud_aio_apache:/mnt/data:rw"
+        ];
+
+        ports = [
+          "127.0.0.1:11000:11000"
+        ];
+      };
+
+      nextcloud-aio-database = {
+        autoStart = true;
+        extraOptions = [
+          "--network=nextcloud"
+          "--read-only"
+          "--shm-size=268435456"
+          "--mount=type=tmpfs,destination=/var/run/postgresql,rw=true"
+        ];
+
+        image = "nextcloud/aio-postgresql";
+
+        environment = {
+          POSTGRES_USER = "nextcloud";
+          POSTGRES_DB = "nextcloud_database";
+          POSTGRES_PASSWORD = "${cfg.databasePassword}";
+          TZ = "${timezone}";
+          PGTZ = "${timezone}";
+        };
+
+        volumes = [
+          "nextcloud_aio_database:/var/lib/postgresql/data:rw"
+          "nextcloud_aio_database_dump:/mnt/data:rw"
+        ];
+      };
+
+      nextcloud-aio-nextcloud = {
+        autoStart = true;
+        extraOptions = [
+          "--network=nextcloud"
+        ];
+
+        image = "nextcloud/aio-nextcloud";
+
+        environment = {
+          TZ = "${timezone}";
+          NEXTCLOUD_DATA_DIR = "/mnt/ncdata";
+          POSTGRES_HOST = "nextcloud-aio-database";
+          POSTGRES_PASSWORD = "${cfg.databasePassword}";
+          POSTGRES_DB = "nextcloud_database";
+          POSTGRES_USER = "nextcloud";
+          REDIS_HOST = "nextcloud-aio-redis";
+          REDIS_HOST_PASSWORD = "${cfg.redisPassword}";
+          NC_DOMAIN = "${nextcloudDomain}";
+          ADMIN_USER = "admin";
+          ADMIN_PASSWORD = "${cfg.nextcloudPassword}";
+          OVERWRITEHOST = "${nextcloudDomain}";
+          OVERWRITEPROTOCOL = "https";
+          CLAMAV_ENABLED = "yes";
+          CLAMAV_HOST = "nextcloud-aio-clamav";
+          ONLYOFFICE_ENABLED = "no";
+          COLLABORA_ENABLED = "no";
+          TALK_ENABLED = "no";
+          UPDATE_NEXTCLOUD_APPS = "no";
+          IMAGINARY_ENABLED = "yes";
+          IMAGINARY_HOST = "nextcloud-aio-imaginary";
+          PHP_UPLOAD_LIMIT = "10G";
+          PHP_MEMORY_LIMIT = "512M";
+          FULLTEXTSEARCH_ENABLED = "yes";
+          FULLTEXTSEARCH_HOST = "nextcloud-aio-fulltextsearch";
+          PHP_MAX_TIME = "3600";
+          ADDITIONAL_APKS = "imagemagick";
+          ADDITIONAL_PHP_EXTENSIONS = "imagick";
+          FULLTEXTSEARCH_PASSWORD = "${cfg.fulltextsearchPassword}";
+          REMOVE_DISABLED_APPS = "yes";
+          APACHE_PORT = "11000";
+          APACHE_IP_BINDING = "127.0.0.1";
+        };
+
+        volumes = [
+          "nextcloud_aio_nextcloud:/var/www/html:rw"
+          "nextcloud_aio_nextcloud_data:/mnt/ncdata:rw"
+        ];
+
+        dependsOn = [
+          "nextcloud-aio-database"
+          "nextcloud-aio-redis"
+        ];
+      };
+
+      nextcloud-aio-redis = {
+        autoStart = true;
+        extraOptions = [
+          "--network=nextcloud"
+          "--read-only"
+        ];
+
+        image = "nextcloud/aio-redis";
+
+        environment = {
+          TZ = "${timezone}";
+          REDIS_HOST_PASSWORD = "${cfg.redisPassword}";
+        };
+
+        volumes = [
+          "nextcloud_aio_redis:/data:rw"
+        ];
+      };
+
+      nextcloud-aio-clamav = {
+        autoStart = true;
+        extraOptions = [
+          "--network=nextcloud"
+          "--read-only"
+          "--read-only-tmpfs"
+          "--mount=type=tmpfs,destination=/var/lock,rw=true"
+          "--mount=type=tmpfs,destination=/var/log/clamav,rw=true"
+        ];
+
+        image = "nextcloud/aio-clamav";
+
+        environment = {
+          TZ = "${timezone}";
+          CLAMD_STARTUP_TIMEOUT = "90";
+        };
+
+        volumes = [
+          "nextcloud_aio_clamav:/var/lib/clamav:rw"
+        ];
+      };
+
+      nextcloud-aio-notify-push = {
+        autoStart = true;
+        extraOptions = [
+          "--network=nextcloud"
+          "--read-only"
+        ];
+
+        image = "nextcloud/aio-notify-push";
+
+        environment = {
+          NC_DOMAIN = "${nextcloudDomain}";
+          NEXTCLOUD_HOST = "nextcloud-aio-nextcloud";
+          REDIS_HOST = "nextcloud-aio-redis";
+          REDIS_HOST_PASSWORD = "${cfg.redisPassword}";
+          POSTGRES_HOST = "nextcloud-aio-database";
+          POSTGRES_PASSWORD = "${cfg.databasePassword}";
+          POSTGRES_DB = "nextcloud_database";
+          POSTGRES_USER = "nextcloud";
+        };
+
+        volumes = [
+          "nextcloud_aio_nextcloud:/nextcloud:ro"
+        ];
+      };
+
+      nextcloud-aio-imaginary = {
+        autoStart = true;
+        extraOptions = [
+          "--network=nextcloud"
+          "--read-only"
+          "--read-only-tmpfs"
+          "--cap-add=SYS_NICE"
+        ];
+
+        image = "nextcloud/aio-imaginary";
+
+        environment = {
+          TZ = "${timezone}";
+        };
+      };
+
+      nextcloud-aio-fulltextsearch = {
+        autoStart = true;
+        extraOptions = [
+          "--network=nextcloud"
+        ];
+
+        image = "nextcloud/aio-fulltextsearch";
+
+        environment = {
+          TZ = "${timezone}";
+          FULLTEXTSEARCH_PASSWORD = "${cfg.fulltextsearchPassword}";
+          ES_JAVA_OPTS = "-Xms512M -Xmx512M";
+          "bootstrap.memory_lock" = "true";
+          "cluster.name" = "nextcloud-aio";
+          "discovery.type" = "single-node";
+          "logger.org.elasticsearch.discovery" = "WARN";
+          "http.port" = "9200";
+          "xpack.license.self_generated.type" = "basic";
+          "xpack.security.enabled" = "false";
+        };
+
+        volumes = [
+          "nextcloud_aio_elasticsearch:/usr/share/elasticsearch/data:rw"
+        ];
       };
     };
 
