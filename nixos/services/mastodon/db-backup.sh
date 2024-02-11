749e0d86d416eee10cda419ec79d2dcd8ffabf6d
Daniel Czerwonk <daniel@routing.rocks>
2024-02-11 08:40:13 +0100

    rename backup files

---
+++ nixos/services/mastodon/db-backup.sh
@@ -0,0 +1,9 @@
+set -e
+
+BACKUP_DIR="/data/backup/mastodon"
+
+mkdir -p $BACKUP_DIR
+
+echo "Backup DB"
+podman exec mastodon-db pg_dumpall -U postgres | gzip > $BACKUP_DIR/db.sql.gz.tmp
+mv $BACKUP_DIR/db.sql.gz.tmp $BACKUP_DIR/db.sql.gz
---
+++ nixos/services/matrix/backup-db.sh
@@ -0,0 +1,9 @@
+set -e
+
+BACKUP_DIR="/data/backup/matrix"
+
+mkdir -p $BACKUP_DIR
+
+echo "Backup DB"
+podman exec matrix-db pg_dumpall --user synapse | gzip > $BACKUP_DIR/db.sql.gz.tmp
+mv $BACKUP_DIR/db.sql.gz.tmp $BACKUP_DIR/db.sql.gz
