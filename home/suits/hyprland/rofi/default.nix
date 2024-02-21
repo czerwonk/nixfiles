b6873f5dc16fbd52e50bbfbb5b7a6f2905ccb4cf
Daniel Czerwonk <daniel@routing.rocks>
2024-02-21 09:04:02 +0100

    small rofi tweaks

--- home/suits/hyprland/rofi/default.nix
+++ home/suits/hyprland/rofi/default.nix
@@ -4,9 +4,22 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
-    plugins = [ pkgs.rofi-emoji ];
+    plugins = with pkgs; [
+      rofi-calc
+      rofi-emoji
+    ];
     terminal = "${pkgs.kitty}/bin/kitty";
     font = "JetBrains Mono";
     theme = ./theme.rasi;
+    extraConfig = {
+      modi = "drun,emoji,calc";
+      show-icons = true;
+      drun-display-format = "{icon} {name}";
+      disable-history = false;
+      display-drun = "   Apps ";
+      display-run = "   Run ";
+      display-emoji = "   Emoji ";
+      display-calc = "   Calc ";
+    };
   };
 }
