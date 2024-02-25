37b9f2b56c915776b44b5247b746854e4eae74e0
Daniel Czerwonk <daniel@routing.rocks>
2024-02-25 13:52:26 +0100

    reduce gnome overhead

--- nixos/iso/default.nix
+++ nixos/iso/default.nix
@@ -14,4 +14,33 @@
   })];
 
   boot.supportedFilesystems = [ "bcachefs" ];
+
+  services.xserver = {
+    desktopManager.gnome = {
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        experimental-features=['scale-monitor-framebuffer']
+      '';
+    };
+  };
+
+  services.gnome = {
+    games.enable = false;
+    core-developer-tools.enable = false;
+  };
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gnome-connections
+    gnome-text-editor
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gnome-maps
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    seahorse # keyring UI
+  ]);
 }
