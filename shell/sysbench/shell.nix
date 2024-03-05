14450466d463c968bb15e621d7372f2a2301509a
Daniel Czerwonk <daniel@routing.rocks>
2024-03-05 10:06:14 +0100

    add nix shell for disk performance tests

---
+++ shell/sysbench/shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    sysbench
+    (pkgs.writeShellScriptBin "run-fileio" ''
+      mode=$1
+      if [ "$1" = "" ]; then
+        mode="rndrw"
+      fi
+
+      sysbench fileio --file-total-size=10G --file-num=512 prepare
+      sysbench fileio --num-threads=1 --file-total-size=10G --file-num=512 --file-test-mode=$mode --time=300 --max-requests=0 --file-extra-flags=direct --file-fsync-freq=1 run
+      sysbench fileio --file-total-size=10G --file-num=512 cleanup 1> /dev/null
+    '')
+  ];
+}
