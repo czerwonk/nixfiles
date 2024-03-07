44a0a7ca2e6bb770be95895402a65e0b21e16135
Daniel Czerwonk <daniel@routing.rocks>
2024-03-07 09:16:07 +0100

    kernel upgrade

--- nixos/zfs.nix
+++ nixos/zfs.nix
@@ -5,9 +5,9 @@
 
   boot.zfs.forceImportRoot = lib.mkDefault false;
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_6_hardened;
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_7_hardened;
   environment.systemPackages = [
-    pkgs.linuxKernel.packages.linux_6_6_hardened.zfs
+    pkgs.linuxKernel.packages.linux_6_7_hardened.zfs
   ];
 
   services.zfs.trim.enable = true;
