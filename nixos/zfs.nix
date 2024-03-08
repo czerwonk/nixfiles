d4aa8b3f4ef8227c4dd000635c832fe3c229360a
Daniel Czerwonk <daniel@routing.rocks>
2024-03-08 12:49:40 +0100

    install sanoid

--- nixos/zfs.nix
+++ nixos/zfs.nix
@@ -8,6 +8,7 @@
   boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_7_hardened;
   environment.systemPackages = [
     pkgs.linuxKernel.packages.linux_6_7_hardened.zfs
+    pkgs.sanoid
   ];
 
   services.zfs.trim.enable = true;
