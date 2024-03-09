bac63d97e89a57e979c5ca0c8ecd6cd19e022406
Daniel Czerwonk <daniel@routing.rocks>
2024-03-09 08:45:38 +0100

    Update immich
    Signed-off-by: Daniel Czerwonk <daniel@routing.rocks>

--- nixos/services/immich/default.nix
+++ nixos/services/immich/default.nix
@@ -4,7 +4,7 @@ with lib;
 
 let
   cfg = config.my.services.immich;
-  version = "v1.98.0";
+  version = "v1.98.1";
   backup = pkgs.writeShellScriptBin "immich-backup" (builtins.readFile ./backup.sh);
   databaseName = "immich";
   databaseUsername = "postgres";
