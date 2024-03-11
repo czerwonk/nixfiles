5fbf5eea3a0c991ab55b693de9fc1489c26bbe47
Daniel Czerwonk <daniel@routing.rocks>
2024-03-11 12:42:31 +0100

    ignore encrypted datasets not encrypted with pool key on boot

--- nixos/hosts/framy/configuration.nix
+++ nixos/hosts/framy/configuration.nix
@@ -13,6 +13,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.zfs.forceImportRoot = true;
+  boot.zfs.requestEncryptionCredentials = [ "zroot" ];
 
   boot.extraModprobeConfig = ''
     options usbserial vendor=0403 product=6001
