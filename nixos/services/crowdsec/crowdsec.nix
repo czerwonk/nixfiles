1168c1bf1fec674e24d964b85897c8ae0cf247ba
Daniel Czerwonk <daniel@routing.rocks>
2024-03-12 23:04:52 +0100

    set start limit

--- nixos/services/crowdsec/crowdsec.nix
+++ nixos/services/crowdsec/crowdsec.nix
@@ -63,6 +63,8 @@ in {
     };
 
     systemd.services.crowdsec.serviceConfig = {
+      StartLimitIntervalSec = 300;
+      StartLimitBurst = 5;
       ExecStartPre = let
         script = pkgs.writeScriptBin "pre-start" ''
           #!${pkgs.runtimeShell}
