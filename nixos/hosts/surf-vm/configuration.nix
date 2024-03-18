9423702e633f000c20285151d24c2bdbcf590262
Daniel Czerwonk <daniel@routing.rocks>
2024-03-18 15:06:21 +0100

    Revert "fix copy paste error"
    This reverts commit 24ed3fdb1d285e424359c10d131b87138dcd395a.

--- nixos/hosts/surf-vm/configuration.nix
+++ nixos/hosts/surf-vm/configuration.nix
@@ -34,7 +34,7 @@
     packages = with pkgs; [
       gnome.gnome-tweaks
       wgnord
-      linuxKernel.packages.linux_6_7_hardened.virtualboxGuestAdditions
+      linuxKernel.packages.linuxKernel.packages.linux_6_7_hardened.virtualboxGuestAdditions
     ];
   };
 
