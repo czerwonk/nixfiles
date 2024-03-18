5f7579e3b125b88e4d42fb1e17272bb4846e2b03
Daniel Czerwonk <daniel@routing.rocks>
2024-03-18 13:02:53 +0100

    sddm

--- nixos/profiles/desktop/plasma.nix
+++ nixos/profiles/desktop/plasma.nix
@@ -2,15 +2,17 @@
 
 {
   disabledModules = [
-    "services/x11/desktop-managers/plasma5.nix"
     "programs/chromium.nix"
     "programs/gnupg.nix"
+    "services/x11/desktop-managers/plasma5.nix"
+    "services/x11/display-managers/sddm.nix"
   ];
 
   imports = [
-    "${inputs.nixpkgs-unstable}/nixos/modules/services/desktop-managers/plasma6.nix"
     "${inputs.nixpkgs-unstable}/nixos/modules/programs/chromium.nix"
     "${inputs.nixpkgs-unstable}/nixos/modules/programs/gnupg.nix"
+    "${inputs.nixpkgs-unstable}/nixos/modules/services/desktop-managers/plasma6.nix"
+    "${inputs.nixpkgs-unstable}/nixos/services/x11/display-managers/sddm.nix"
   ];
 
   services.xserver = {
