5502ffac1abea16f24bf5827e0d70eee2dbdfbc4
Daniel Czerwonk <daniel@routing.rocks>
2024-04-02 12:56:21 +0200

    simplify audit rules

--- nixos/hardening/audit.nix
+++ nixos/hardening/audit.nix
@@ -26,19 +26,25 @@
     rules = [
       "-i" # ignore missing users or files
 
-      # failed file access
-      "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=-1 -k failed_access"
-      "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=-1 -k failed_access"
-      "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=-1 -k failed_access"
-      "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=-1 -k failed_access"
-
       # access modifications
       "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=-1 -k perm_mod"
-      "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=-1 -k perm_mod"
       "-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=-1 -k perm_mod"
-      "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=-1 -k perm_mod"
       "-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=-1 -k perm_mod"
-      "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=-1 -k perm_mod"
+
+      # deletions
+      "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=-1 -k delete"
+
+      # failed file creation
+      "-a always,exit -F arch=b64 -S mkdir,creat,link,symlink,mknod,mknodat,linkat,symlinkat -F exit=-EACCES -F auid>=1000 -F auid!=-1 -k failed_file_create"
+      "-a always,exit -F arch=b64 -S mkdir,link,symlink,mkdirat -F exit=-EPERM -F auid>=1000 -F auid!=-1 -k failed_file_create"
+
+      # failed file access
+      "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=-1 -k failed_file_access"
+      "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=-1 -k failed_file_access"
+
+      # failed file modifications
+      "-a always,exit -F arch=b64 -S rename -S renameat -S truncate -S chmod -S setxattr -S lsetxattr -S removexattr -S lremovexattr -F exit=-EACCES -F auid>=1000 -F auid!=-1 -k failed_file_mod"
+      "-a always,exit -F arch=b64 -S rename -S renameat -S truncate -S chmod -S setxattr -S lsetxattr -S removexattr -S lremovexattr -F exit=-EPERM -F auid>=1000 -F auid!=-1 -k failed_file_mod"
 
       # file integrity monitoring
       "-w /etc/sudoers -p wa -k file_integrity"
@@ -63,13 +69,8 @@
       # executions
       "-a always,exclude -F msgtype=CWD"
       "-a always,exit -F arch=b64 -S execve -F euid=0 -F auid>=1000 -F auid!=-1 -S execve -k sudo"
-      "-a always,exit -F arch=b32 -S execve -F euid=0 -F auid>=1000 -F auid!=-1 -S execve -k sudo"
       "-a always,exit -F arch=b64 -S mount -S umount2 -F auid!=-1 -k mount"
       "-a always,exit -F arch=b64 -S mknod -S mknodat -k specialfiles"
-
-      # deletions
-      "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=-1 -k delete"
-      "-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=-1 -k delete"
 
       # modules
       "-a always,exit -F perm=x -F auid!=-1 -F path=/run/current-system/sw/bin/insmod -k modules"
@@ -82,6 +83,9 @@
       "-a always,exit -F arch=b64 -S ptrace -F a0=0x5 -k data_injection"
       "-a always,exit -F arch=b64 -S ptrace -F a0=0x6 -k register_injection"
       "-a always,exit -F arch=b64 -S ptrace -k tracing"
+
+      # 32bit (all systems are 64bit)
+      "-a always,exit -F arch=b32 -S all -k 32bit"
     ];
   };
   security.auditd.enable = true;
