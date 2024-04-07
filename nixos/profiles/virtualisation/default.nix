8accff0f9de72aabe3dd6a05a006441123872d3d
Daniel Czerwonk <daniel@routing.rocks>
2024-04-07 20:21:09 +0200

    do not start libvirtd on boot

--- nixos/profiles/virtualisation/default.nix
+++ nixos/profiles/virtualisation/default.nix
@@ -1,7 +1,14 @@
-{ username, ... }:
+{ lib, username, ... }:
 
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    onBoot = lib.mkDefault "ignore";
+    qemu = {
+      swtpm.enable = true;
+    };
+  };
+
   programs.virt-manager.enable = true;
 
   users.users.${username}.extraGroups = [ "libvirtd" ];
