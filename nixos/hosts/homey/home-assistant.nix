cc78a5d38b0f4b4ad87d623213a0da4f82a13602
Daniel Czerwonk <daniel@routing.rocks>
2024-04-17 08:12:13 +0200

    initial mosquito setup

--- nixos/hosts/homey/home-assistant.nix
+++ nixos/hosts/homey/home-assistant.nix
@@ -29,6 +29,24 @@
     };
   };
 
+  services.mosquitto = {
+    enable = true;
+    listeners = [
+      {
+        users = {
+          tasmota = {
+            acl = [ "readwrite #" ];
+            hashedPassword = "$7$101$fortkoOCO04mWhwY$Dky/QZbn5pBUHle0tFwLT+Pu9CFN767tyrIp9VSyY6y/irk8TtT6rwyG3XdyI5jbOUTqqJHYZ3VHhOmOaIEHNQ==";
+          };
+          home-assistant = {
+            acl = [ "readwrite #" ];
+            hashedPassword = "$7$101$yTCVaxI32b4Tq3K1$6W87/4eCBjbGw5w9QbS9YRt5oubaSMcvymIUd1xEbO8IaldJcLaJVMp7rFPN0COx6iLWDaSk8TICOxw07OOhiw==";
+          };
+        };
+      }
+    ];
+  };
+
   services.caddy.virtualHosts."home.routing.rocks".extraConfig = ''
     import private
 
