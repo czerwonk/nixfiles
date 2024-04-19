6c8d5bc37a6cd4c6f148168db7cf757ce3f84f45
Daniel Czerwonk <daniel@routing.rocks>
2024-04-19 08:27:42 +0200

    import qemu module

--- nixos/profiles/qemu-vm/default.nix
+++ nixos/profiles/qemu-vm/default.nix
@@ -1,12 +1,17 @@
-{ modulesPath, ... }:
+{ modulesPath, inputs, ... }:
 
 {
   imports = [ 
     (modulesPath + "/profiles/qemu-guest.nix")
+    "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
   ];
 
   boot.kernelModules = [
     "uinput"
+  ];
+
+  virtualisation.qemu.options = [
+    "-vga qxl -device virtio-serial-pci -spice port=5930,disable-ticketing=on -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent"
   ];
 
   services.spice-vdagentd.enable = true;
