ef825637944c348a637330626f763ab8b6647609
Daniel Czerwonk <daniel@routing.rocks>
2024-05-16 07:49:58 +0200

    move internal display right (scrapy)

--- nixos/hosts/scrapy/home.nix
+++ nixos/hosts/scrapy/home.nix
@@ -11,6 +11,7 @@
   profiles.hyprland = {
     backlightDevice = "intel_backlight";
     extraConfig = ''
+      monitor=HDMI-A-1,preferred,0x0,1.0
       monitor=eDP-1,preferred,auto,1.6
     '';
   };
