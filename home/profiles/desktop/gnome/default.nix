4162134f5408895ec908be63272eb1d129d52c4a
Daniel Czerwonk <daniel@routing.rocks>
2024-05-21 17:55:39 +0200

    configure pop-shell

--- home/profiles/desktop/gnome/default.nix
+++ home/profiles/desktop/gnome/default.nix
@@ -4,5 +4,25 @@
     ./theme.nix
   ];
 
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      disabled-extensions = "disabled";
+      enabled-extensions = [
+        "pop-shell@system76.com"
+      ];
+      had-bluetooth-devices-setup = true;
+      remember-mount-password = false;
+    };
+    "org/gnome/shell/extensions/pop-shell" = {
+      active-hint = true;
+      active-hint-border-radius = 10;
+      gap-inner = 1;
+      gap-outer = 1;
+      tile-by-default = true;
+      activate-launcher = [ "<Super>space" ];
+    };
+  };
+
   services.gpg-agent.pinentryFlavor = "gnome3";
 }
