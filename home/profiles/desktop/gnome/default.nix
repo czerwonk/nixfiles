94d8e974c4cabf4a1959824daafa2072c71f69ec
Daniel Czerwonk <daniel@routing.rocks>
2024-05-22 07:24:04 +0200

    define custom shortcut for terminal

--- home/profiles/desktop/gnome/default.nix
+++ home/profiles/desktop/gnome/default.nix
@@ -1,3 +1,5 @@
+{ pkgs, lib, ... }:
+
 {
   imports = [
     ../common.nix
@@ -61,6 +63,15 @@
     };
     "org/gnome/shell/keybindings" = {
       show-screenshot-ui = [ "<Super><Shift>S" ];
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>t";
+      command = "${lib.getExe pkgs.kitty}";
+      name = "Open Terminal";
     };
   };
 
