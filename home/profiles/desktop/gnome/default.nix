8be417f891631f79f3cd2db96c7b0ea4e761db88
Daniel Czerwonk <daniel@routing.rocks>
2024-05-22 09:41:56 +0200

    enable switch to workspace 5-9

--- home/profiles/desktop/gnome/default.nix
+++ home/profiles/desktop/gnome/default.nix
@@ -40,6 +40,10 @@
       switch-to-application-3 = [];
       switch-to-application-4 = [];
       switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
       switch-input-source = [];
       switch-input-source-backward = [];
     };
