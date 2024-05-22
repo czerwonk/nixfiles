e14d91c720e392daa9c1c94bf61489fde5c36564
Daniel Czerwonk <daniel@routing.rocks>
2024-05-22 21:10:47 +0200

    switch to 24.05 release branch freshly branched off

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Daniel Czerwonk's Nix configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
