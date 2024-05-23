3435a31ccebd2eb6835ee8b975243e53163f849e
Daniel Czerwonk <daniel@routing.rocks>
2024-05-23 14:44:31 +0200

    use more eye pleasing color

--- overlays.nix
+++ overlays.nix
@@ -48,7 +48,7 @@ in {
       gnomeExtensions = super.gnomeExtensions // {
         pop-shell = super.gnomeExtensions.pop-shell.overrideAttrs (oldAttrs: {
           preFixup = oldAttrs.preFixup + ''
-            echo '.pop-shell-search-element:select{ color: #C0A36E !important; }' >> $out/share/gnome-shell/extensions/pop-shell@system76.com/light.css
+            echo '.pop-shell-search-element:select{ color: #DCD7BA !important; }' >> $out/share/gnome-shell/extensions/pop-shell@system76.com/light.css
           '';
         });
       };
