bf9c88df159a428db31721ed67de725a67b33b33
Daniel Czerwonk <daniel@routing.rocks>
2024-05-23 15:48:55 +0200

    switch to home-manager release channel

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
