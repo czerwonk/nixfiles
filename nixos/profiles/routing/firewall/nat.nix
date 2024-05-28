fe47f959f641633d36eed3241ff793f5d6bd02f0
Daniel Czerwonk <daniel@routing.rocks>
2024-05-28 16:49:21 +0200

    cleanup

--- nixos/profiles/routing/firewall/nat.nix
+++ nixos/profiles/routing/firewall/nat.nix
@@ -8,7 +8,6 @@ in {
   networking.nftables.tables."nixos-nat".content = lib.mkAfter ''
     chain post {
       type nat hook postrouting priority srcnat - 1; policy accept;
-      iifname { "podman*", "wg*" } oifname "${nat.externalInterface}" snat ip to ${nat.externalIP}
       ${lib.flip lib.concatMapStrings config.networking.nat.internalInterfaces (iface: ''
         iifname "${iface}" oifname "${nat.externalInterface}" snat ip to ${nat.externalIP}
       '')}
