6800194ccfc0c3147d0f8c1a9049f0a5050da69b
Daniel Czerwonk <daniel@routing.rocks>
2024-06-20 11:15:56 +0200

    cleanup

--- home/programs/zsh/history.zsh
+++ home/programs/zsh/history.zsh
@@ -4,7 +4,7 @@ autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 
-bindkey "^[[A" history-beginning-search-backward-end
-bindkey "^[[B" history-beginning-search-forward-end
+bindkey '^[[A' history-beginning-search-backward-end
+bindkey '^[[B' history-beginning-search-forward-end
 bindkey '^p'      history-search-backward
 bindkey '^n'      history-search-forward
