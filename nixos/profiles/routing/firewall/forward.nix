5471e19f7b66ecd3d84a5774e683ab959de2f47c
Daniel Czerwonk <daniel@routing.rocks>
2024-06-27 15:49:43 +0200

    block timestamp requests

--- nixos/profiles/routing/firewall/forward.nix
+++ nixos/profiles/routing/firewall/forward.nix
@@ -8,6 +8,7 @@
       type filter hook forward priority filter; policy drop;
       ct status dnat accept comment "allow port forward"
       ct state vmap { established : accept, related : accept }
+      icmp type == { timestamp-request, timestamp-reply } jump drop-forward comment "Accept all ICMP messages except timestamp requests"
       icmpv6 type == { router-renumbering, 139 } jump drop-forward comment "Accept all ICMPv6 messages except renumbering and node information queries (type 139).  See RFC 4890, section 4.3."
       ${lib.flip lib.concatMapStrings config.networking.firewall.trustedInterfaces (iface: ''
         iifname "${iface}" accept
