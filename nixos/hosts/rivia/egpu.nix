98cd2d42fdd86af19c23a3aaf190b439a7d0f622
Daniel Czerwonk <daniel@routing.rocks>
2024-08-07 09:45:39 +0200

    prepare use of rocm

--- nixos/hosts/rivia/egpu.nix
+++ nixos/hosts/rivia/egpu.nix
@@ -1,7 +1,17 @@
 { pkgs, ... }:
 
 {
+  systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  ];
+
+  hardware.opengl.extraPackages = with pkgs; [
+    rocmPackages.clr.icd
+  ];
+
   environment.systemPackages = [
     pkgs.all-ways-egpu
   ];
+
+  my.services.ai.acceleration = "rocm";
 }
