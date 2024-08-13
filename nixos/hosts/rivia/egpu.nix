bea94142b2937957ad6072c37223ee11b9afda16
Daniel Czerwonk <daniel@routing.rocks>
2024-08-13 14:27:05 +0200

    tune egpu

--- nixos/hosts/rivia/egpu.nix
+++ nixos/hosts/rivia/egpu.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
+  kernelParams = [
+    "amdgpu.pcie_gen_cap=0x40000" # Force AMD GPU to use full width (optional)
+  ];
+
   systemd.tmpfiles.rules = [
     "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
   ];
