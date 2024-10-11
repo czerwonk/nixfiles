46bec1c0f6792a52dbe20a9083245eb629f0a741
Daniel Czerwonk <daniel@routing.rocks>
2024-10-11 13:30:12 +0200

    remove udev rules, already set by adb

--- nixos/profiles/desktop/android.nix
+++ nixos/profiles/desktop/android.nix
@@ -4,9 +4,5 @@
   programs.adb.enable = true;
   users.users.${username}.extraGroups = ["adbusers"];
 
-  services.udev.packages = [
-    pkgs.android-udev-rules
-  ];
-
   environment.systemPackages = [ pkgs.android-studio ];
 }
