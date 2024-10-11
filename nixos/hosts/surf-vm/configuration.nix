8802cc7ec54063dfe8969dae0a65415898ce2fb3
Daniel Czerwonk <daniel@routing.rocks>
2024-10-11 16:46:47 +0200

    add icons for firejail apps

--- nixos/hosts/surf-vm/configuration.nix
+++ nixos/hosts/surf-vm/configuration.nix
@@ -57,6 +57,29 @@
     };
   };
 
+  environment.systemPackages = [
+    (
+      let packages = with pkgs; [
+        brave
+        firefox
+      ];
+
+      in
+        pkgs.runCommand "firejail-icons" {
+          preferLocalBuild = true;
+          allowSubstitutes = false;
+          meta.priority = -1;
+        } ''
+          mkdir -p "$out/share/icons"
+          ${lib.concatLines (map (pkg: ''
+            tar -C "${pkg}" -c share/icons -h --mode 0755 -f - | tar -C "$out" -xf -
+          '') packages)}
+          find "$out/" -type f -print0 | xargs -0 chmod 0444
+          find "$out/" -type d -print0 | xargs -0 chmod 0555
+        ''
+    )
+  ];
+
   services.dnsmasq.enable = false;
 
   services.xserver.desktopManager.pantheon.enable = true;
