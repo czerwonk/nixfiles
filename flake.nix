d64e76bcbae44e991289c254b0eecbbeae101a4c
Daniel Czerwonk <daniel@routing.rocks>
2024-10-21 12:28:15 +0200

    switch to unstable small

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
