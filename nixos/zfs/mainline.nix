663059d36de09a29f10b2b1c8fc6dbe6d570bc37
Daniel Czerwonk <daniel@routing.rocks>
2024-10-25 18:29:57 +0200

    ugrade kernel

--- nixos/zfs/mainline.nix
+++ nixos/zfs/mainline.nix
@@ -7,8 +7,8 @@
 
   boot.zfs.package = pkgs.zfs;
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_10;
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_11;
   environment.systemPackages = with pkgs; [
-    linuxKernel.packages.linux_6_10.zfs
+    linuxKernel.packages.linux_6_11.zfs
   ];
 }
