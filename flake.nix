20f38b908dda0a340fb603c327490f3157e0c472
Daniel Czerwonk <daniel@routing.rocks>
2024-11-22 14:30:31 +0100

    use release branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Daniel Brendgen-Czerwonk's Nix configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
