fdcd85a4527fb9141b2ea0fb0a9543c09be4cf48
Daniel Czerwonk <daniel@routing.rocks>
2024-11-28 21:32:31 +0100

    install kitty

--- darwin/hosts/nara/home.nix
+++ darwin/hosts/nara/home.nix
@@ -1,7 +1,20 @@
-{ extraHomeModules, ... }:
+{ pkgs, lib, config, extraHomeModules, ... }:
 
 {
   imports = [
     ../../../home/osx
+    ../../../home/programs/kitty
   ] ++ extraHomeModules;
+
+  home.packages = with pkgs; [
+    wl-clipboard
+  ];
+
+  programs.zsh.shellAliases = {
+    ycode = "${lib.getExe pkgs.yubikey-manager} oath accounts code | ${lib.getExe config.programs.fzf.package}";
+  };
+
+  programs.tmux.mouse = true;
+
+  services.gpg-agent.enable = true;
 }
