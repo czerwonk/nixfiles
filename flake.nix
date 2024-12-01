a784df0e904a5818480a0c1abd33bf2201256cb1
Daniel Czerwonk <daniel@routing.rocks>
2024-12-01 09:21:18 +0100

    upgrade os x

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
-    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
@@ -19,7 +19,7 @@
     };
 
     home-manager-darwin = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
 
