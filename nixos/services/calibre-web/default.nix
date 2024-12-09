4c9006942955734f2daa1ed5401ee134db705ce7
Daniel Czerwonk <daniel@routing.rocks>
2024-12-09 10:19:02 +0100

    add new calibre-web server

--- nixos/services/calibre-web/default.nix
+++ nixos/services/calibre-web/default.nix
@@ -11,17 +11,23 @@ in {
     my.services.calibre-web = {
       enable = mkEnableOption "Calibre Web";
 
-      libraryDir = mkOption {
+      booksLibraryDir = mkOption {
         description = "Local path to data directory containing book files";
         type = types.str;
         default = "/data/media/Books";
+      };
+
+      mangaLibraryDir = mkOption {
+        description = "Local path to data directory containing manga files";
+        type = types.str;
+        default = "/data/media/Manga";
       };
     };
   };
 
   config = mkIf cfg.enable {
     virtualisation.oci-containers.containers = {
-      calibre-web = {
+      calibre-web-books = {
         image = "lscr.io/linuxserver/calibre-web:${version}";
 
         autoStart = true;
@@ -38,8 +44,30 @@ in {
         ports = [ "127.0.0.1:8083:8083" ];
 
         volumes = [
-          "${cfg.libraryDir}:/books:ro"
-          "calibre_config:/config"
+          "${cfg.booksLibraryDir}:/books:ro"
+          "calibre_books_config:/config"
+        ];
+      };
+
+      calibre-web-manga = {
+        image = "lscr.io/linuxserver/calibre-web:${version}";
+
+        autoStart = true;
+        extraOptions = [
+          "--runtime=${pkgs.gvisor}/bin/runsc"
+        ];
+
+        environment = {
+          PUID = "1000";
+          PGID = "1000";
+          TZ = "Europe/Berlin";
+        };
+
+        ports = [ "127.0.0.1:8086:8083" ];
+
+        volumes = [
+          "${cfg.mangaLibraryDir}:/books:ro"
+          "calibre_manga_config:/config"
         ];
       };
     };
@@ -50,10 +78,17 @@ in {
       reverse_proxy * 127.0.0.1:8083
     '';
 
+    services.caddy.virtualHosts."manga.routing.rocks".extraConfig = ''
+      import private
+
+      reverse_proxy * 127.0.0.1:8086
+    '';
+
     services.restic.backups.calibre-web = {
       initialize = true;
       paths = [
-        "/var/lib/containers/storage/volumes/calibre_config"
+        "/var/lib/containers/storage/volumes/calibre_books_config"
+        "/var/lib/containers/storage/volumes/calibre_manga_config"
       ];
       pruneOpts = [
         "--keep-daily 7"
