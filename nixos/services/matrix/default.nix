d9c6d69b23956517e0391e3210983cdd787ca16e
Daniel Czerwonk <daniel@routing.rocks>
2024-12-11 16:18:23 +0100

    upgrade synapse

--- nixos/services/matrix/default.nix
+++ nixos/services/matrix/default.nix
@@ -4,7 +4,7 @@ with lib;
 
 let
   cfg = config.my.services.matrix;
-  version = "v1.120.2";
+  version = "v1.121.0";
   backup = pkgs.writeShellScriptBin "matrix-db-backup" (builtins.readFile ./db-backup.sh);
 
 in {
