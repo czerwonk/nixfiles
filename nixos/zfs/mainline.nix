9695722146424156494b8d03ce249a7ea66dcfcd
Daniel Czerwonk <daniel@routing.rocks>
2024-12-15 13:35:38 +0100

    kernel upgrade (6.12 with ZFS 2.2.7)

--- nixos/zfs/mainline.nix
+++ nixos/zfs/mainline.nix
@@ -7,8 +7,8 @@
 
   boot.zfs.package = pkgs.zfs;
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_11;
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_12;
   environment.systemPackages = with pkgs; [
-    linuxKernel.packages.linux_6_11.zfs
+    linuxKernel.packages.linux_6_12.zfs
   ];
 }
