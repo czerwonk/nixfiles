0b8e56f9008e85922a125fd0485abe62b0876c04
Daniel Czerwonk <daniel@routing.rocks>
2024-12-27 09:53:37 +0100

    add extraConfig for ghostty to configure darwin config

--- darwin/hosts/nara/home.nix
+++ darwin/hosts/nara/home.nix
@@ -6,4 +6,9 @@
     ../../../home/profiles/desktop/common.nix
     ../../../home/profiles/devops
   ] ++ extraHomeModules;
+
+  my.programs.ghostty.extraConfig = ''
+    macos-titlebar-style = hidden
+    background-opacity = 0.99
+  '';
 }
--- home/programs/ghostty/default.nix
+++ home/programs/ghostty/default.nix
@@ -1,11 +1,46 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
-{
-  home = {
-    packages = with pkgs; [
-      ghostty
-      jetbrains-mono
-    ];
-    file.".config/ghostty/config".text = builtins.readFile ./config;
+with lib;
+
+let
+  cfg = config.my.programs.ghostty;
+
+in {
+  options = {
+    my.programs.ghostty = {
+      extraConfig = mkOption {
+        type = types.str;
+        default = "";
+      };
+    };
+  };
+
+  config = {
+    home = {
+      packages = with pkgs; [
+        ghostty
+        jetbrains-mono
+      ];
+      file.".config/ghostty/config".text = ''
+        font-family = JetBrains Mono
+        font-size = 12
+        confirm-close-surface = false
+        shell-integration = zsh
+        window-decoration = false
+
+        foreground = E1D9D1
+        background = 1F1F28
+        palette = 1=C34043
+        palette = 2=76946A
+        palette = 3=E6C384
+        palette = 4=7FB4CA
+        palette = 11=FF9E3B
+        palette = 31=A3D4D5
+        palette = 76=98BB6C
+        palette = 234=1F1F28
+
+        ${cfg.extraConfig}
+      '';
+    };
   };
 }
