58a69920fe487bb1c634ad306d6cd6317ea674f8
Daniel Czerwonk <daniel@routing.rocks>
2025-01-28 08:14:23 +0100

    upgrade immich

--- nixos/services/immich/default.nix
+++ nixos/services/immich/default.nix
@@ -4,7 +4,7 @@ with lib;
 
 let
   cfg = config.my.services.immich;
-  version = "v1.125.2";
+  version = "v1.125.6";
   backup = pkgs.writeShellScriptBin "immich-backup" (builtins.readFile ./backup.sh);
   databaseName = "immich";
   databaseUsername = "postgres";
