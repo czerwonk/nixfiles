205c172ba1cff7a7bfdece5028faea14ae578583
Daniel Czerwonk <daniel@routing.rocks>
2025-02-09 13:15:26 +0100

    upgrade snacks

--- pkgs/snacks-nvim/default.nix
+++ pkgs/snacks-nvim/default.nix
@@ -2,12 +2,12 @@
 
 pkgs.vimUtils.buildVimPlugin {
   pname = "snacks.nvim";
-  version = "v2.19.0";
+  version = "v2.20.0";
   src = fetchFromGitHub {
     owner = "folke";
     repo = "snacks.nvim";
-    rev = "789f161de12021b9304dc36231283a8b2ecabbe9";
-    sha256 = "sha256-+Af4epvkGStCv2tlWWoI2MZSCNafkpgVahUlkrt9fmc=";
+    rev = "76a5dcfb318d623022dada44c66453d9cb9a6eaa";
+    sha256 = "sha256-YUjTuY47fWnHd9/z6WqFD0biW+wn9zLLsOVJibwpgKw=";
   };
   meta.homepage = "https://github.com/folke/snacks.nvim/";
 }
