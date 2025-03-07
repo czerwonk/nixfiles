9c954ba431117afa14ed8392f6d30791204969b4
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-03-07 13:26:10 +0100

    remove snack explorer keymap

--- home/programs/vim/plugins/lua/snacks.lua
+++ home/programs/vim/plugins/lua/snacks.lua
@@ -25,7 +25,6 @@ vim.notify = notify
 vim.keymap.set('n', '<c-t>', function() Snacks.terminal() end, { desc = 'Terminal (cwd)' })
 vim.keymap.set('t', '<C-/>', '<cmd>close<cr>', { desc = 'Hide Terminal' })
 vim.keymap.set('n', '<leader>n', function() Snacks.notifier.show_history() end, { desc = 'Notification History' })
-vim.keymap.set('n', '<C-n>', function() Snacks.explorer() end, { desc = 'Toggle Explorer' })
 vim.keymap.set('n', '<leader>u', function() Snacks.picker.undo() end, { desc = 'Undo Picker' })
 vim.keymap.set('n', '<leader>i', function() Snacks.picker.icons() end, { desc = 'Icon Picker' })
 vim.keymap.set('n', '<leader>ft', function() Snacks.picker.todo_comments() end, { desc = 'TODO Comments' })
