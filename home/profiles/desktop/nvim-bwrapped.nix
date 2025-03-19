54e464481d66923e2c499abffeaef49cfe9cc9f0
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-03-19 21:02:36 +0100

    use final package

--- home/profiles/desktop/nvim-bwrapped.nix
+++ home/profiles/desktop/nvim-bwrapped.nix
@@ -1,11 +1,10 @@
-{ pkgs, lib, username, ... }:
+{ pkgs, lib, config, ... }:
 
 let
   nvim-bwrapped = pkgs.writeShellScriptBin "nvim-bwrapped" ''
     ${lib.getExe pkgs.bubblewrap} --ro-bind /usr /usr \
                                   --ro-bind /nix /nix \
-                                  --ro-bind "/etc/profiles/per-user/${username}" "/etc/profiles/per-user/${username}" \
-                                  --ro-bind /etc/ssl /etc/ssl \
+                                  --ro-bind /etc/ /etc \
                                   --ro-bind /run /run \
                                   --ro-bind "$HOME" "$HOME" \
                                   --bind /tmp /tmp \
@@ -15,7 +14,7 @@ let
                                   --bind "$HOME/.cache" "$HOME/.cache" \
                                   --proc /proc \
                                   --dev /dev \
-      /etc/profiles/per-user/${username}/bin/nvim $@
+      ${lib.getExe config.programs.neovim.finalPackage} $@
   '';
 
 in {
