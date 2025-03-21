7cab645ceb286435808afb6b751601ac1d63d70c
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-03-21 15:59:08 +0100

    small tweaks

--- home/programs/vim/sandbox.nix
+++ home/programs/vim/sandbox.nix
@@ -4,23 +4,23 @@ with lib;
 
 let
   nvim-bwrapped = pkgs.writeShellScriptBin "nvim-bwrapped" ''
-    ${lib.getExe pkgs.bubblewrap} --ro-bind /usr /usr \
-                                  --ro-bind /nix /nix \
-                                  --ro-bind /bin /bin \
-                                  --ro-bind /etc /etc \
-                                  --ro-bind /run/current-system/sw/bin /run/current-system/sw/bin \
-                                  --ro-bind "$HOME" "$HOME" \
-                                  --tmpfs "$HOME/.ssh" \
-                                  --ro-bind "$HOME/.ssh/known_hosts" "$HOME/.ssh/known_hosts" \
-                                  --bind "$(pwd)" "$(pwd)" \
-                                  --bind "$HOME/.local/share/nvim" "$HOME/.local/share/nvim" \
-                                  --bind "$HOME/.local/state/nvim" "$HOME/.local/state/nvim" \
-                                  --bind "$HOME/.cache" "$HOME/.cache" \
-                                  --bind "$HOME/go/pkg" "$HOME/go/pkg" \
-                                  --bind "$XDG_RUNTIME_DIR" "$XDG_RUNTIME_DIR" \
-                                  --tmpfs /tmp \
-                                  --proc /proc \
-                                  --dev /dev \
+    exec ${lib.getExe pkgs.bubblewrap} --ro-bind /usr /usr \
+                                       --ro-bind /nix /nix \
+                                       --ro-bind /bin /bin \
+                                       --ro-bind /etc /etc \
+                                       --ro-bind /run/current-system/sw/bin /run/current-system/sw/bin \
+                                       --ro-bind "$HOME" "$HOME" \
+                                       --tmpfs "$HOME/.ssh" \
+                                       --ro-bind "$HOME/.ssh/known_hosts" "$HOME/.ssh/known_hosts" \
+                                       --bind "$(pwd)" "$(pwd)" \
+                                       --bind "$HOME/.local/share/nvim" "$HOME/.local/share/nvim" \
+                                       --bind "$HOME/.local/state/nvim" "$HOME/.local/state/nvim" \
+                                       --bind "$HOME/.cache" "$HOME/.cache" \
+                                       --bind "$HOME/go/pkg" "$HOME/go/pkg" \
+                                       --bind "$XDG_RUNTIME_DIR" "$XDG_RUNTIME_DIR" \
+                                       --tmpfs /tmp \
+                                       --proc /proc \
+                                       --dev /dev \
       ${lib.getExe config.programs.neovim.finalPackage} "$@"
   '';
 
