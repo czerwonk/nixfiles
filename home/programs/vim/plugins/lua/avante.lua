b68a2a7b82f811b936a7d7a095d88f9aab8f01ec
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-03-21 16:53:10 +0100

    replace add_provider by add_source_provider in blink config (former is deprecated)

--- home/programs/vim/plugins/lua/avante.lua
+++ home/programs/vim/plugins/lua/avante.lua
@@ -32,19 +32,19 @@ require('which-key').add({
 });
 
 local blink = require('blink.cmp')
-blink.add_provider('avante_commands', {
+blink.add_source_provider('avante_commands', {
   name = 'avante_commands',
   module = 'blink.compat.source',
   score_offset = 90, -- show at a higher priority than lsp
   opts = {},
 })
-blink.add_provider('avante_files', {
+blink.add_source_provider('avante_files', {
   name = 'avante_files',
   module = 'blink.compat.source',
   score_offset = 100, -- show at a higher priority than lsp
   opts = {},
 })
-blink.add_provider('avante_mentions', {
+blink.add_source_provider('avante_mentions', {
   name = 'avante_mentions',
   module = 'blink.compat.source',
   score_offset = 1000, -- show at a higher priority than lsp
