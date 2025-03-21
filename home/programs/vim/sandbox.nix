7171191111cb4810cfdd4086fecb15a428632cc0
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-03-21 08:38:34 +0100

    Use tmpfs as walkaruond for ssh config issue inside of sandbox

--- home/programs/vim/sandbox.nix
+++ home/programs/vim/sandbox.nix
@@ -10,12 +10,14 @@ let
                                        --ro-bind /etc /etc \
                                        --ro-bind /run/current-system/sw/bin /run/current-system/sw/bin \
                                        --ro-bind "$HOME" "$HOME" \
-                                       --bind /tmp /tmp \
+                                       --tmpfs "$HOME/.ssh" \
+                                       --ro-bind "$HOME/.ssh/known_hosts" "$HOME/.ssh/known_hosts" \
                                        --bind "$(pwd)" "$(pwd)" \
                                        --bind "$HOME/.local/share/nvim" "$HOME/.local/share/nvim" \
                                        --bind "$HOME/.local/state/nvim" "$HOME/.local/state/nvim" \
                                        --bind "$HOME/.cache" "$HOME/.cache" \
                                        --bind "$XDG_RUNTIME_DIR" "$XDG_RUNTIME_DIR" \
+                                       --tmpfs /tmp \
                                        --proc /proc \
                                        --dev /dev \
       ${lib.getExe config.programs.neovim.finalPackage} "$@"
