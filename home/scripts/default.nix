8980622df8102d7c913a89a07e11dd7f3cf78c3a
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-03-31 10:11:12 +0200

    use final package for default nvim exe in nvim-tmux

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -25,7 +25,7 @@ in {
   options = {
     my.scripts.neovimExe = mkOption {
       type = types.str;
-      default = "${lib.getExe pkgs.neovim}";
+      default = "${lib.getExe config.programs.neovim.finalPackage}";
       description = "The neovim executable to use";
     };
   };
