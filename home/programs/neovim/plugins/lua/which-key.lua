a7101fa770399890a69ed33349234078708fdd88
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-04-03 14:26:17 +0200

    remove unused which-key extras

--- home/programs/neovim/plugins/lua/which-key.lua
+++ home/programs/neovim/plugins/lua/which-key.lua
@@ -13,13 +13,6 @@ wk.setup {
   { "<leader>r", group = "Refactoring" },
   { "<leader>t", group = "Test" },
   { "<leader>x", group = "Diagnostics" },
-  {
-    "<leader>b",
-      group = "buffer",
-      expand = function()
-        return require("which-key.extras").expand.buf()
-      end,
-    },
 {
     "<leader>w",
       group = "windows",
