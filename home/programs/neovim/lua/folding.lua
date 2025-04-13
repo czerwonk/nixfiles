39d49bb47730e5b6f2dcd82e17af707e6396a692
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-04-13 10:52:32 +0200

    move chars to folding config

--- home/programs/neovim/lua/folding.lua
+++ home/programs/neovim/lua/folding.lua
@@ -3,3 +3,10 @@ vim.opt.foldmethod = "indent"
 vim.opt.foldnestmax = 3
 vim.opt.foldlevel = 99
 vim.opt.foldminlines = 3
+
+vim.opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  fold = " ",
+  foldsep = " ",
+}
