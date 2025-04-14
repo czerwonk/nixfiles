dcf5485390652800aed37fd38127ca4f59695e31
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-04-14 08:47:24 +0200

    use tree-sitter for folding

--- home/programs/neovim/lua/folding.lua
+++ home/programs/neovim/lua/folding.lua
@@ -1,12 +1,22 @@
+_G.foldexpr = function()
+  local buf = vim.api.nvim_get_current_buf()
+
+  if vim.b[buf].ts_folds == nil then
+    -- as long as we don't have a filetype, don't bother
+    -- checking if treesitter is available (it won't)
+    if vim.bo[buf].filetype == "" then
+      return "0"
+    end
+
+    vim.b[buf].ts_folds = pcall(vim.treesitter.get_parser, buf)
+  end
+
+  return vim.b[buf].ts_folds and vim.treesitter.foldexpr() or "0"
+end
+
 vim.opt.foldenable = true
-vim.opt.foldmethod = "indent"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.foldexpr()"
 vim.opt.foldnestmax = 3
 vim.opt.foldlevel = 99
 vim.opt.foldminlines = 3
-
-vim.opt.fillchars = {
-  foldopen = "",
-  foldclose = "",
-  fold = " ",
-  foldsep = " ",
-}
