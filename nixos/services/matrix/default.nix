9f310e95aff63e5cbf4bddad1b6feb2d42d339a9
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-05-06 16:47:01 +0200

    upgrade mastodon and matrix

--- nixos/services/matrix/default.nix
+++ nixos/services/matrix/default.nix
@@ -9,7 +9,7 @@ with lib;
 
 let
   cfg = config.my.services.matrix;
-  version = "v1.128.0";
+  version = "v1.129.0";
   backup = pkgs.writeShellScriptBin "matrix-db-backup" (builtins.readFile ./db-backup.sh);
 
 in
