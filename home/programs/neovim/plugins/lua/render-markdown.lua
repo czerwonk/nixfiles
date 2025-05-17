741cbbb6abc3df115215454573eb27223d9ce5a4
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-05-17 13:49:42 +0200

    use builtin blink config for render-markdown

--- home/programs/neovim/plugins/lua/render-markdown.lua
+++ home/programs/neovim/plugins/lua/render-markdown.lua
@@ -1,16 +1,11 @@
 require('render-markdown').setup {
   completions = {
+    blink = {
+      enabled = true
+    },
     lsp = {
       enabled = true
     }
   },
   file_types = { 'markdown', 'Avante' },
 };
-
-local blink = require('blink.cmp')
-blink.add_source_provider('markdown', {
-  name = 'RenderMarkdown',
-  module = 'render-markdown.integ.blink',
-  fallbacks = { 'lsp' },
-})
-blink.add_filetype_source('markdown', 'markdown')
