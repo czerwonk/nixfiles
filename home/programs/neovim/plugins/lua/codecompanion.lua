f570fe4870bff75756d5b198c9064910556dacb7
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-05-23 11:49:22 +0200

    improve notification of codecompanion status

--- home/programs/neovim/plugins/lua/codecompanion.lua
+++ home/programs/neovim/plugins/lua/codecompanion.lua
@@ -108,25 +108,15 @@ require('codecompanion').setup {
 local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
 local group = vim.api.nvim_create_augroup('CodeCompanionFidgetHooks', { clear = true })
 vim.api.nvim_create_autocmd({ 'User' }, {
-  pattern = 'CodeCompanion*',
+  pattern = { 'CodeCompanionRequestStarted', 'CodeCompanionRequestStreaming', 'CodeCompanionRequestFinished' },
   group = group,
   callback = function(request)
-    if request.match == 'CodeCompanionChatSubmitted' then
-      return
-    end
-
-    local msg
-
-    msg = '[CodeCompanion] ' .. request.match:gsub('CodeCompanion', '')
+    local msg = '[CodeCompanion] ' .. request.match:gsub('CodeCompanion', '')
 
     vim.notify(msg, 'info', {
       timeout = 1000,
-      keep = function()
-        return not vim
-          .iter({ 'Finished', 'Opened', 'Hidden', 'Closed', 'Cleared', 'Created' })
-          :fold(false, function(acc, cond)
-            return acc or vim.endswith(request.match, cond)
-          end)
+      keep = function ()
+        return not vim.endswith(request.match, 'Finished')
       end,
       id = 'code_companion_status',
       title = 'Code Companion Status',
