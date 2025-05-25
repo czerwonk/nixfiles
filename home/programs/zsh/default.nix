9e9a86c8923223b1344c1954bff3cda7259d84f9
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-05-25 13:06:53 +0200

    cleanup

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -17,8 +17,8 @@
       bindkey "^[[1;3C" forward-word
       bindkey "^[^[[D"  backward-word
       bindkey "^[^[[C"  forward-word
-      bindkey "^[[1;5D"  backward-word
-      bindkey "^[[1;5C"  forward-word
+      bindkey "^[[1;5D" backward-word
+      bindkey "^[[1;5C" forward-word
       bindkey "^[^?"    backward-kill-word
 
       autoload zmv
