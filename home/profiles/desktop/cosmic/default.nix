80753faae9eb8b072586d71574a3f38507e393af
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-06-01 11:38:32 +0200

    set default options for cosmic

--- home/profiles/desktop/cosmic/default.nix
+++ home/profiles/desktop/cosmic/default.nix
@@ -1,5 +1,38 @@
 { pkgs, ... }:
 
 {
+  xdg.configFile = {
+    "cosmic/com.system76.CosmicTheme.Mode/v1/is_dark" = {
+      enable = true;
+      force = true;
+      text = "true";
+    };
+    "cosmic/com.system76.CosmicTheme.Dark/v1/gaps" = {
+      enable = true;
+      force = true;
+      text = "(0, 0)";
+    };
+    "cosmic/com.system76.CosmicComp/v1/active_hint" = {
+      enable = true;
+      force = true;
+      text = "false";
+    };
+    "cosmic/com.system76.CosmicComp/v1/autotile" = {
+      enable = true;
+      force = true;
+      text = "true";
+    };
+    "cosmic/com.system76.CosmicComp/v1/workspaces" = {
+      enable = true;
+      force = true;
+      text = ''
+        (
+          workspace_mode: OutputBound,
+          workspace_layout: Horizontal,
+        )
+      '';
+    };
+  };
+
   services.gpg-agent.pinentry.package = pkgs.pinentry-gtk2;
 }
--- nixos/profiles/desktop/cosmic.nix
+++ nixos/profiles/desktop/cosmic.nix
@@ -11,6 +11,7 @@
   };
 
   environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
     ELECTRON_OZONE_PLATFORM_HINT = "wayland";
   };
 }
