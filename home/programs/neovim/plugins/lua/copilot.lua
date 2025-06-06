cde6acfc01bafd6cf512a5658905e3eca6d62c84
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-06-06 16:31:09 +0200

    ensure I am able to insert tabs by using shift + tab

--- home/programs/neovim/plugins/lua/copilot.lua
+++ home/programs/neovim/plugins/lua/copilot.lua
@@ -5,7 +5,7 @@ require('copilot').setup({
     debounce = 75,
     max_lines = 10,
     keymap = {
-      accept = '<Tab>',
+      accept = '<C-j>',
       accept_word = '<C-l>',
       accept_line = '<C-k>',
       next = '<M-]>',
@@ -18,6 +18,16 @@ require('copilot').setup({
   },
   copilot_node_command = 'node',
 })
+
+vim.keymap.set("i", "<Tab>", function()
+  if require("copilot.suggestion").is_visible() then
+    require("copilot.suggestion").accept()
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+  end
+end, { silent = true })
+
+vim.keymap.set("i", "<S-Tab>", "<Tab>", { remap = false })
 
 vim.keymap.set('n', '<leader>C', function()
   require("copilot.suggestion").toggle_auto_trigger()
