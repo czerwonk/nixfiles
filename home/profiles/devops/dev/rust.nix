4c54ffa44e5e4ae142eedea0de0bb6806ffb88d2
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-06-13 09:48:35 +0200

    ensure openssl can be found

--- home/profiles/devops/dev/rust.nix
+++ home/profiles/devops/dev/rust.nix
@@ -5,8 +5,12 @@
     packages = with pkgs; [
       cargo
       clippy
+      pkg-config
       rustc
       rustfmt
     ];
+    sessionVariables = {
+      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    };
   };
 }
