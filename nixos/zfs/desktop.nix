ed2e2e7207642c1edba4cd4b70c4b64e3007919d
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-06-30 08:59:30 +0200

    ZFS 2.3.3 is available is staging, so no unstable kernel and module is needed

--- nixos/zfs/desktop.nix
+++ nixos/zfs/desktop.nix
@@ -5,10 +5,10 @@
     ./common.nix
   ];
 
-  boot.zfs.package = pkgs.unstable.zfs;
-  boot.kernelPackages = lib.mkForce pkgs.unstable.linuxKernel.packages.linux_6_15;
+  boot.zfs.package = pkgs.zfs;
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_15;
 
   environment.systemPackages = with pkgs; [
-    unstable.linuxKernel.packages.linux_6_15.zfs_2_3
+    linuxKernel.packages.linux_6_15.zfs_2_3
   ];
 }
