2b5a90bdf6ef31afca41ea27b6e3125cc2900729
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-08-10 08:14:39 +0200

    upgrade ollama

--- overlays/ollama-rocm-apu.nix
+++ overlays/ollama-rocm-apu.nix
@@ -1,17 +1,14 @@
 self: super: {
   ollama-rocm = super.ollama-rocm.overrideAttrs (oldAttrs: rec {
     pname = "ollama-rocm-apu";
-    version = "0.9.3";
+    version = "0.11.3";
 
     src = super.fetchFromGitHub {
       owner = "rjmalagon";
       repo = "ollama-linux-amd-apu";
       rev = "v${version}";
-      # This hash needs to be updated when fetching
-      hash = "sha256-h/VT/anJRmVRnD+n89/KkSiMy5Ft9N9YmjAesUBPpNo=";
+      hash = "sha256-ol0ivSd2O6CVlc3ki/tDOVePt5mWWyGyYAobmSKGPSE=";
     };
-
-    # Additional build inputs or modifications may be needed
-    # depending on the differences between the repositories
   });
 }
+
