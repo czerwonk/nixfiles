3052f3736ad954fbd3b48f055cfb76e5bd98c8e5
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-08-18 16:00:19 +0200

    use default config for tempo

--- nixos/services/monitoring/tempo.nix
+++ nixos/services/monitoring/tempo.nix
@@ -16,18 +16,6 @@ in
           http_listen_port = 3200;
         };
 
-        distributor = {
-          receivers = {
-            otlp = {
-              protocols = {
-                grpc = {
-                  endpoint = "127.0.0.1:14317"; # non-defualt port
-                };
-              };
-            };
-          };
-        };
-
         storage.trace = {
           backend = "local";
           local.path = "${dataDir}/traces";
