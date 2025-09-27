adb4a8993a4cb77e9823f623dc33e18d9cf43c17
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-09-27 11:56:37 +0200

    disable power settings for ISO

--- nixos/iso/gnome/default.nix
+++ nixos/iso/gnome/default.nix
@@ -11,6 +11,22 @@
       extraGSettingsOverrides = ''
         [org.gnome.mutter]
         experimental-features=['scale-monitor-framebuffer']
+
+        # Disable automatic suspend on AC power
+        [org.gnome.settings-daemon.plugins.power]
+        sleep-inactive-ac-type='nothing'
+        sleep-inactive-battery-type='nothing'
+        sleep-inactive-ac-timeout=0
+        sleep-inactive-battery-timeout=0
+
+        # Disable screen blanking/idle
+        [org.gnome.desktop.session]
+        idle-delay=uint32 0
+
+        # Disable screensaver
+        [org.gnome.desktop.screensaver]
+        lock-enabled=false
+        lock-delay=uint32 0
       '';
     };
   };
