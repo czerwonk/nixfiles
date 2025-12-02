8c51d4a9788be27c6db455f3a44935f9097ffe3a
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-12-02 11:08:36 +0100

    update darwin url

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     darwin = {
-      url = "github:lnl7/nix-darwin/nix-darwin-25.05";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
 
