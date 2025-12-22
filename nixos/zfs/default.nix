d5f119521ab753e6efaa3fe1ab4063731922282b
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-12-22 13:40:03 +0100

    try 6.18 LTS on servers

--- nixos/zfs/default.nix
+++ nixos/zfs/default.nix
@@ -5,8 +5,9 @@
     ./common.nix
   ];
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_12_hardened;
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_18;
+
   environment.systemPackages = with pkgs; [
-    linuxKernel.packages.linux_6_12_hardened.zfs_2_4
+    linuxKernel.packages.linux_6_18.zfs_2_4
   ];
 }
