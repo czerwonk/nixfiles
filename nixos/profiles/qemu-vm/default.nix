177bcae49d6fa95a52c633876bfc7d480d1f9c7d
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-01-23 11:45:36 +0100

    ensure virtio

--- nixos/profiles/qemu-vm/default.nix
+++ nixos/profiles/qemu-vm/default.nix
@@ -5,12 +5,18 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.kernelModules = [ "uinput" ];
+  boot.kernelModules = [
+    "uinput"
+    "virtio_gpu"
+  ];
   boot.initrd.kernelModules = [ "virtio_gpu" ];
 
   services.spice-vdagentd.enable = true;
 
   services.qemuGuest.enable = true;
 
-  hardware.graphics.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true; # If needed for 32-bit apps
+  };
 }
