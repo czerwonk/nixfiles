2bb8bb819e227b1202898ab3d0a48f74983218b1
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-01-27 13:24:05 +0100

    install ollama

--- nixos/hosts/kyoto/configuration.nix
+++ nixos/hosts/kyoto/configuration.nix
@@ -58,15 +58,15 @@
     autoStart = false;
   };
 
-  # services.ollama = {
-  #   enable = true;
-  #   package = pkgs.ollama-rocm;
-  #   acceleration = "rocm";
-  #   rocmOverrideGfx = "11.0.0";
-  #   environmentVariables = {
-  #     OLLAMA_FLASH_ATTENTION = "true";
-  #     OLLAMA_KV_CACHE_TYPE = "q8_0";
-  #     OLLAMA_DEBUG = "0";
-  #   };
-  # };
+  services.ollama = {
+    enable = true;
+    package = pkgs.ollama-rocm;
+    acceleration = "rocm";
+    rocmOverrideGfx = "11.0.0";
+    environmentVariables = {
+      OLLAMA_FLASH_ATTENTION = "true";
+      OLLAMA_KV_CACHE_TYPE = "q8_0";
+      OLLAMA_DEBUG = "0";
+    };
+  };
 }
