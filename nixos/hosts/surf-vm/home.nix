cce76238a1457515575d7cb647ee04cbf73ab15c
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-02-05 18:27:31 +0100

    update home.nix for surf-vm

--- nixos/hosts/surf-vm/home.nix
+++ nixos/hosts/surf-vm/home.nix
@@ -2,17 +2,20 @@
 
 {
   imports = [
-    ../../../home/profiles/core/minimal-packages.nix
-    ../../../home/programs/bat
-    ../../../home/programs/fzf
-    ../../../home/programs/ghostty
-    ../../../home/programs/hakanai
-    ../../../home/programs/neovim
-    ../../../home/programs/zed
-    ../../../home/programs/zsh
+    ../../../home/profiles/core
     ../../../home/profiles/desktop/librewolf.nix
   ]
   ++ extraHomeModules;
+
+  programs.ghostty.enable = true;
+  programs.zed-editor.enable = true;
+
+  programs.git.enable = false;
+  programs.gpg.enable = false;
+  programs.oh-my-posh.enable = false;
+  programs.ssh.enable = false;
+  programs.tmux.enable = false;
+  programs.zoxide.enable = false;
 
   home = {
     username = username;
