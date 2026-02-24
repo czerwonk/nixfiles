eb5002b51189bba39714ae02018a960b02378a32
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-02-24 15:07:04 +0100

    upgrade mastodon

--- nixos/services/mastodon/default.nix
+++ nixos/services/mastodon/default.nix
@@ -9,7 +9,7 @@ with lib;
 
 let
   cfg = config.my.services.mastodon;
-  version = "4.4.13";
+  version = "4.4.14";
   backup = pkgs.writeShellScriptBin "mastodon-db-backup" (builtins.readFile ./db-backup.sh);
   cleanup = pkgs.writeShellScriptBin "mastodon-cleanup" (builtins.readFile ./cleanup.sh);
   env = {
