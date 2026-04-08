fc25fc731c498c8a398ff43e714cb167dc445984
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-04-08 17:46:17 +0200

    set routed traffic notrack

--- nixos/profiles/container/default.nix
+++ nixos/profiles/container/default.nix
@@ -24,31 +24,4 @@
   environment.sessionVariables = {
     DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
   };
-
-  systemd.services.netavark-fixup = {
-    description = "Remove unscoped ct state invalid drop from netavark FORWARD chain";
-    after = [ "podman.service" ];
-    wantedBy = [ "multi-user.target" ];
-    path = [
-      pkgs.nftables
-      pkgs.gawk
-    ];
-    serviceConfig = {
-      Type = "simple";
-      Restart = "always";
-      RestartSec = 5;
-      ExecStart = pkgs.writeShellScript "netavark-fixup" ''
-        while true; do
-          handle=$(nft -a list chain inet netavark FORWARD 2>/dev/null \
-            | grep 'ct state invalid drop' \
-            | awk '{print $NF}')
-          if [ -n "$handle" ]; then
-            nft delete rule inet netavark FORWARD handle "$handle" && \
-              echo "Removed ct state invalid drop (handle $handle)"
-          fi
-          sleep 5
-        done
-      '';
-    };
-  };
 }
