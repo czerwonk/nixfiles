bcd5c515041aebfcb080282e2040c25f34e58ffe
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-04-24 15:14:44 +0200

    add udev rule for davinci speed editor

--- nixos/profiles/desktop/media.nix
+++ nixos/profiles/desktop/media.nix
@@ -12,7 +12,9 @@
   users.groups.plugdev = { };
   users.users.${username}.extraGroups = [ "plugdev" ];
 
+  # udev rules for davinci speed editor
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTRS{idVendor}=="1edb", MODE="0666", GROUP="plugdev"
+    SUBSYSTEM=="hidraw", KERNEL=="hidraw*", ATTRS{idVendor}=="1edb", MODE="0666"
   '';
 }
