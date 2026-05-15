2f6b5dee377829fa91f9d0ceae83a4c89027964e
daniel <daniel@routing.rocks>
2026-05-15 12:07:47 +0200

    upgrade desktops to linux 7.x using ZFS 2.4.2

--- nixos/zfs/desktop.nix
+++ nixos/zfs/desktop.nix
@@ -5,9 +5,9 @@
     ./common.nix
   ];
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_18;
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_7_0;
 
   environment.systemPackages = with pkgs; [
-    linuxKernel.packages.linux_6_18.zfs_2_4
+    linuxKernel.packages.linux_7_0.zfs_2_4
   ];
 }
