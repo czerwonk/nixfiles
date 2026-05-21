158c9ad4893c4c6c53b16838ea43bb555452b33b
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-05-21 08:15:28 +0200

    security headers

--- nixos/hosts/bb1-dus/static-web/danalog-pics.nix
+++ nixos/hosts/bb1-dus/static-web/danalog-pics.nix
@@ -8,5 +8,24 @@
     file_server {
       precompressed
     }
+
+    header {
+      Content-Security-Policy "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-ancestors 'none'; form-action 'self';"
+
+      Access-Control-Allow-Origin "null"
+      Cross-Origin-Embedder-Policy "require-corp"
+      Cross-Origin-Opener-Policy "same-origin"
+      Cross-Origin-Resource-Policy "same-origin"
+
+      Strict-Transport-Security "max-age=63072000; includeSubDomains; preload"
+
+      X-Frame-Options "DENY"
+      X-Content-Type-Options "nosniff"
+
+      Referrer-Policy "no-referrer"
+
+      Permissions-Policy "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()"
+      -Server
+    }
   '';
 }
