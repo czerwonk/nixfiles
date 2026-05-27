5bada6a458d192c13d3d6b1abe654586acfc59eb
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-05-27 10:56:17 +0200

    use compatible pop-shell version

--- overlays/gnome-extensions.nix
+++ overlays/gnome-extensions.nix
@@ -1,11 +1,16 @@
 self: super: {
   gnomeExtensions = super.gnomeExtensions // {
     pop-shell = super.gnomeExtensions.pop-shell.overrideAttrs (oldAttrs: {
-      preFixup =
-        oldAttrs.preFixup
-        + ''
-          echo '.pop-shell-search-element:select{ color: #DCD7BA !important; }' >> $out/share/gnome-shell/extensions/pop-shell@system76.com/light.css
-        '';
+      version = "1.2.0-unstable-gnome50";
+      src = super.fetchFromGitHub {
+        owner = "pop-os";
+        repo = "shell";
+        rev = "7898b65c20735057faf0797f8ed056704ca55f0d";
+        hash = "sha256-MmHoOxymo0QSRbRcSbFiv82+QWAwIwXwg/wyGQGVYiI=";
+      };
+      preFixup = oldAttrs.preFixup + ''
+        echo '.pop-shell-search-element:select{ color: #DCD7BA !important; }' >> $out/share/gnome-shell/extensions/pop-shell@system76.com/light.css
+      '';
     });
   };
 }
