2841bc00671c80eb5260a54c9283afb5a2da8bac
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-07-08 09:10:31 +0200

    downgrade kernel to 6.18 until openzfs update

--- nixos/zfs/desktop.nix
+++ nixos/zfs/desktop.nix
@@ -5,21 +5,9 @@
     ./common.nix
   ];
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_7_0;
-  # boot.kernelPackages = pkgs.linuxPackagesFor (
-  #   pkgs.linux_7_0.override {
-  #     argsOverride = rec {
-  #       src = pkgs.fetchurl {
-  #         url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
-  #         sha256 = "sha256:08vm18wx6399phzgr3wz94yga3ab4fyca79445ygvbspm904996b";
-  #       };
-  #       version = "7.0.6";
-  #       modDirVersion = "7.0.6";
-  #     };
-  #   }
-  # );
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_18;
 
   environment.systemPackages = with pkgs; [
-    linuxKernel.packages.linux_7_0.zfs_2_4
+    linuxKernel.packages.linux_6_18.zfs_2_4
   ];
 }
