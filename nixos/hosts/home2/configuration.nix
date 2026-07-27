679c0e08796888f72a1415b716c0e4150c0b39a8
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-07-27 20:45:01 +0200

    get rid of the temp. changes

--- nixos/hosts/home2/configuration.nix
+++ nixos/hosts/home2/configuration.nix
@@ -9,10 +9,8 @@
     ../../profiles/server
     ../../profiles/webserver
     ./home-assistant.nix
-    #./unifi.nix
+    ./unifi.nix
   ];
-
-  nix.settings.trusted-users = [ "daniel" ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
