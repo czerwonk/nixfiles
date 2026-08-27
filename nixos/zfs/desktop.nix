311eb81d57971d556b058acb5b2e36756a10028a
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-08-27 15:03:10 +0200

    kernel upgrade

--- nixos/zfs/desktop.nix
+++ nixos/zfs/desktop.nix
@@ -5,9 +5,9 @@
     ./common.nix
   ];
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_18;
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_7_2;
 
   environment.systemPackages = with pkgs; [
-    linuxKernel.packages.linux_6_18.zfs_2_4
+    linuxKernel.packages.linux_7_2.zfs_2_4
   ];
 }
