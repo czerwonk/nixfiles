6824ab58bac4955893e223123fc0fc88e5378e44
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-09-21 17:19:01 +0200

    import default-applications.nix

--- home/profiles/desktop/cosmic/default.nix
+++ home/profiles/desktop/cosmic/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ../common.nix
+    ./default-applications.nix
   ];
 
   xdg.configFile = {
--- nixos/hosts/kyoto/home.nix
+++ nixos/hosts/kyoto/home.nix
@@ -4,7 +4,6 @@
   imports = [
     ../../../home/linux.nix
     ../../../home/profiles/desktop/cosmic
-    ../../../home/profiles/desktop/cosmic/default-applications.nix
     ../../../home/profiles/desktop/workstation.nix
     ../../../home/profiles/devops
     ../../../home/profiles/static-web
