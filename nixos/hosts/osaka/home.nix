b10a433309c6418ede210e13498254706eb8995d
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-09-21 17:14:23 +0200

    define default apps

--- nixos/hosts/osaka/home.nix
+++ nixos/hosts/osaka/home.nix
@@ -4,6 +4,7 @@
   imports = [
     ../../../home/linux.nix
     ../../../home/profiles/desktop/cosmic
+    ../../../home/profiles/desktop/cosmic/default-applications.nix
     ../../../home/profiles/desktop/workstation.nix
     ../../../home/profiles/devops
   ]
